2023488e6afb8b9997499a95161cce007e168361
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2019-10-07 16:23:50 +0100

    Add SNPP data creation back in Previous commit removed data creation script for the SNPP mortality curves. This version handles raw data rather than processed.

---
+++ input_data_scripts/mortality/snpp_mortality_curves.R
@@ -0,0 +1,32 @@
+#Mortality Curves
+
+mort_curve_file <- "Q:/Teams/D&PA/Data/population_projections/ons_snpp/2016-based (May 2018)/model_inputs/2016 snpp sya mortality rates.csv"
+national_file <- "Q:/Teams/D&PA/Data/population_projections/ons_npp/2016-based NPP/model_inputs/national asmrs.csv"
+over90_file <- "Q:/Teams/D&PA/Data/population_projections/ons_npp/2016-based NPP/model_inputs/NPP deaths over 90.csv"
+
+ons_mort <- fread(mort_curve_file) %>%
+  gather(year, death_rate, 5:29) %>%
+  filter(year == 2017)%>%
+  mutate(sex = ifelse(sex=="M","male","female")) %>%
+  select(gss_code, sex, age, death_rate)
+
+national_mort <- fread(national_file) %>%
+  mutate(age = ifelse(age == "Birth", -1, age),
+         age = as.numeric(age),
+         age = age+1) %>%
+  filter(age < 90) %>%
+  mutate(death_rate = mortality_rate/100000) %>%
+  mutate(sex = ifelse(sex=="M","male","female")) %>%
+  select(-mortality_rate)
+
+over90s <- fread(over90_file) %>%
+  mutate(death_rate = deaths/pop) %>%
+  mutate(sex = ifelse(sex=="M","male","female")) %>%
+  select(-pop, -deaths)
+
+ons_mort <- rbind(ons_mort, national_mort, over90s) %>%
+  mutate(year = 2017) %>%
+  select(gss_code, sex, age, year, death_rate)
+
+saveRDS(ons_mort, "input_data/mortality/ons_asmr_curves.rds" )
+
