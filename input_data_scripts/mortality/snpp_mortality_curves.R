dea495c39aba7eea520170321353ceb565c5741e
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2019-10-11 12:36:06 +0100

    Comments

--- input_data_scripts/mortality/snpp_mortality_curves.R
+++ input_data_scripts/mortality/snpp_mortality_curves.R
@@ -6,12 +6,14 @@ mort_curve_file <- "Q:/Teams/D&PA/Data/population_projections/ons_snpp/2016-base
 national_file <- "Q:/Teams/D&PA/Data/population_projections/ons_npp/2016-based NPP/model_inputs/national asmrs.csv"
 over90_file <- "Q:/Teams/D&PA/Data/population_projections/ons_npp/2016-based NPP/model_inputs/NPP deaths over 90.csv"
 
+#Data for English LAs
 ons_mort <- data.table::fread(mort_curve_file) %>%
   gather(year, death_rate, 5:29) %>%
   filter(year == 2017)%>%
   mutate(sex = ifelse(sex=="M","male","female")) %>%
   select(gss_code, sex, age, death_rate)
 
+#Data for Wales, Scotland and NI
 #npp mortality data is published as deaths per 100k of population
 #Also ages are offset by -1 year of age
 #Death data are provided up to 125 so data can safely be filter to < age 90
@@ -32,6 +34,8 @@ over90s <- data.table::fread(over90_file) %>%
 
 national_mort <- rbind(national_mort, over90s)
 
+#Wales LA data is the Welsh national rates applied to each LA
+#TODO: Can we find the actual data for Welshj LAs? 
 welsh_gss_codes <- c(paste0("W0600000",1:6),paste0("W0600000",8:9),paste0("W060000",10:16),paste0("W060000",18:24))
 wales <- list()
 for(w in seq(welsh_gss_codes)){
@@ -42,11 +46,11 @@ wales <- data.table::rbindlist(wales)
 
 national_mort <- filter(national_mort, gss_code != "W92000004")
 
+#Recode - takes a simple average when rates have to to be aggregated
 ons_mort <- rbind(ons_mort, national_mort, wales) %>%
   mutate(year = 2017) %>%
   select(gss_code, sex, age, year, death_rate) %>%
   recode_gss_to_2011(col_geog = "gss_code", col_aggregation = c("gss_code", "sex", "age", "year"), fun = list(mean))
-
 
 dir.create("input_data/mortality", recursive = TRUE, showWarnings = FALSE)
 saveRDS(ons_mort, "input_data/mortality/ons_asmr_curves.rds" )
