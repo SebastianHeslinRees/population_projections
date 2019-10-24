ca232ae47f422cc0066539ce0a559b2a0d37417a
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2019-10-24 12:25:58 +0100

    tests for scaling functions fertility and mortality

--- model_code/popmodules/R/calculate_mean_from_backseries.R
+++ model_code/popmodules/R/calculate_mean_from_backseries.R
@@ -6,6 +6,7 @@
 #' @param years_to_avg numeric. Number of years data to include in the average
 #' @param last_data_year numeric. The final year of data to include in the average
 #' @param data_col Character. The name of the column containing the data
+#' @param col_aggregation Character. The columns to group_by
 #'
 #' @return A data frame of mortality probabilities, fertility rates, or
 #' international migration flows/rates.
@@ -15,7 +16,7 @@
 #'
 #' @export
 
-calculate_mean_from_backseries <- function(data_backseries, years_to_avg, last_data_year, data_col, col_aggregation = c("gss_code","sex","age")){
+calculate_mean_from_backseries <- function(data_backseries, years_to_avg, last_data_year, data_col, col_aggregation = c("gss_code","sex")){
 
   assert_that(is.data.frame(data_backseries),
               msg="calc_trend_rate expects that data_backseries is a dataframe")
@@ -26,7 +27,7 @@ calculate_mean_from_backseries <- function(data_backseries, years_to_avg, last_d
   assert_that(is.character(data_col),
               msg="calc_trend_rate expects that data_col is a column")
   assert_that(data_col %in% names(data_backseries),
-              msg = "in calc_trend_rate the rtae_col variable must be the name of a column in the data_backseries dataframe")
+              msg = "in calc_trend_rate the rate_col variable must be the name of a column in the data_backseries dataframe")
 
   back_years <- c((last_data_year - years_to_avg + 1):last_data_year)
 
