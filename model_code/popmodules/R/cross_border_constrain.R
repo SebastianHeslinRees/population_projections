c94e77db17d2631d89ad2373c20d90047f9f35ed
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2019-10-29 16:26:15 +0000

    bug fixes to domestic and births constraints

--- model_code/popmodules/R/cross_border_constrain.R
+++ model_code/popmodules/R/cross_border_constrain.R
@@ -13,43 +13,64 @@
 #' @return A data frame of scaled domestic flows by authority, sex and
 #'   single year of age.
 
-cross_border_constrain <- function(domestic_flow, constraint, col_flow = "flow")
+cross_border_constrain <- function(domestic_flow, in_constraint, out_constraint, col_flow = "flow"){
   
   out_flow <- filter(domestic_flow, substr(gss_out,1,1) == "E" & substr(gss_in,1,1) != "E")
   in_flow <- filter(domestic_flow, substr(gss_out,1,1) != "E" & substr(gss_in,1,1) == "E")
   
   #Out Flow
   out_scaling <- out_flow %>%
+    rename(cross_out = col_flow) %>%
     group_by(year, sex, age) %>%
-    summarise(flow = sum(flow)) %>%
-    get_scaling_factors(out_flow, constraint, col_popn=col_flow)
+    summarise(cross_out = sum(cross_out)) %>%
+    ungroup() %>%
+    get_scaling_factors(out_constraint, col_popn="cross_out")
   
   scaled_out <- out_flow %>%
-    left_join(scaling, by=c("year","sex","age")) %>%
+    left_join(out_scaling, by=c("year","sex","age")) %>%
+    rename(flow = col_flow) %>%
     mutate(scaled = flow * scaling) %>%
-    ungroup() %>%
-    rename(scaled = col_flow) %>%
+    rename(!!col_flow := flow) %>%
     select(names(domestic_flow)) 
   
   #In flow
   in_scaling <- in_flow %>%
+    rename(cross_in = col_flow) %>%
     group_by(year, sex, age) %>%
-    summarise(flow = sum(flow))%>%
+    summarise(cross_in = sum(cross_in))%>%
     ungroup() %>%
-    get_scaling_factors(out_flow, constraint, col_popn=col_flow)
+    get_scaling_factors(in_constraint, col_popn="cross_in")
   
   scaled_in <- in_flow %>%
-    left_join(scaling, by=c("year","sex","age")) %>%
+    left_join(in_scaling, by=c("year","sex","age")) %>%
+    rename(flow = col_flow) %>%
     mutate(scaled = flow * scaling) %>%
-    rename(scaled = col_flow) %>%
+    rename(!!col_flow := flow) %>%
     select(names(domestic_flow)) 
   
   #Put it back together
-  scaled_flows <- filter(domestic_flow, substr(gss_out,1,1) == "E" & substr(gss_in,1,1) == "E") %>%
-    rbind(scaled_out, scaled_in)
   
+  #TODO Whats a cleverer/better way to do this
+  #Not setdiff it feels like it'll introduce errors
+  #Also, this is slow
+  pairs <- expand.grid(a = c("N","S","W"),b = c("N","S","W"),
+                       stringsAsFactors = F) %>%
+    rbind(data.frame(a="E",b="E"))
+  dont_scale <- list()
+  for(i in seq(nrow(pairs))){
+    a <- pairs[i,1]
+    b <- pairs[i,2]
+    dont_scale[[i]] <- filter(domestic_flow, substr(gss_out,1,1) == a & substr(gss_in,1,1) == b)
+  }
+  dont_scale <- data.table::rbindlist(dont_scale)
+  
+  scaled_flows <- rbind(scaled_in, scaled_out, dont_scale) %>%
+    data.frame()
+  
+  testthat::expect_equal(nrow(out_flow),nrow(scaled_out))
+  testthat::expect_equal(nrow(in_flow),nrow(scaled_in))
   testthat::expect_equal(nrow(scaled_flows),nrow(domestic_flow))
   
   return(scaled_flows)
   
-  }
+}
