192868a3eb0e48a1409b5b503070590fa213f3e8
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2019-10-30 12:04:23 +0000

    bug fixes

--- model_code/popmodules/R/project_rates_npp.R
+++ model_code/popmodules/R/project_rates_npp.R
@@ -2,7 +2,7 @@
 #'
 #' Applies the national rate of change in mortality/fertility to a set of local
 #' authority ASMRs/ASFRs to produce a mortality/fertility probability trajectory.
-#' This takes the projection for the first projection year as an input, and 
+#' This takes the projection for the first projection year as an input, and
 #' extends it to the required number of projection years
 #'
 #' @param jump_off_rates Dataframe. A set of LA/age/sex rates for the first projection year
@@ -23,15 +23,15 @@
 project_rates_npp <- function(jump_off_rates, rate_col, rate_trajectory_filepath, first_proj_yr, n_proj_yr, npp_var="2018_principal"){
 
   rate_trajectory <- readRDS(rate_trajectory_filepath)
-  
+
   #Test/validate
-  check_validate_proj_mort_rates(jump_off_rates, rate_col, rate_trajectory, first_proj_yr, n_proj_yr, npp_var)
+  check_validate_proj_rates_npp(jump_off_rates, rate_col, rate_trajectory, first_proj_yr, n_proj_yr, npp_var)
   final_projection_year <- first_proj_yr + n_proj_yr -1
   assert_that(all((first_proj_yr + 1):final_projection_year %in% rate_trajectory$year))
-  
+
   # calculate the rate changes relative to the first year, and apply this to the
   # jump off rate to calulate the rest of the projection years
-  
+
   rates <- rate_trajectory %>%
     filter(variant == npp_var) %>%
     filter(year > first_proj_yr) %>%
@@ -50,7 +50,7 @@ project_rates_npp <- function(jump_off_rates, rate_col, rate_trajectory_filepath
     rbind(jump_off_rates) %>%
     arrange(gss_code,sex,age,year) %>%
     as.data.frame()
-  
+
   validate_population(rates, col_aggregation = c("gss_code", "sex", "age", "year"),
                       col_data = "rate")
 
@@ -63,7 +63,7 @@ project_rates_npp <- function(jump_off_rates, rate_col, rate_trajectory_filepath
 
 # Function to check that the input to project_fertility_rates is all legal
 
-check_validate_proj_mort_rates <- function(jump_off_rates,
+check_validate_proj_rates_npp <- function(jump_off_rates,
                                            rate_col,
                                            rate_trajectory,
                                            first_proj_yr,
@@ -86,7 +86,7 @@ check_validate_proj_mort_rates <- function(jump_off_rates,
 
   #TODO find out why these fall over
   validate_population(jump_off_rates)
-  validate_population(rate_trajectory, col_aggregation = c("sex","age","year","variant"))
+  #validate_population(rate_trajectory, col_aggregation = c("sex","age","year","variant"))
   validate_join_population(jump_off_rates, rate_trajectory, cols_common_aggregation = c("sex", "age", "year"))
 
 
--- model_code/popmodules/R/scaled_mortality_curve.R
+++ model_code/popmodules/R/scaled_mortality_curve.R
@@ -1,4 +1,4 @@
-#' Calculate average mortality probabilities based on past data and assign to 
+#' Calculate average mortality probabilities based on past data and assign to
 #' first projection year
 #'
 #' Compares past rates by LA and sex to an input
@@ -29,25 +29,30 @@
 
 scaled_mortality_curve <- function(popn_mye_path, births_mye_path, deaths_mye_path, target_curves_filepath,
                                    last_data_year, n_years_to_avg, avg_or_trend, data_col="deaths", output_col){
-  
+
   #TODO change the validation to an lapply
   validate_scaled_mortality_curve_filetype(popn_mye_path, births_mye_path, deaths_mye_path, target_curves_filepath)
-  
+
   population <- data.frame(readRDS(popn_mye_path))
   births <- data.frame(readRDS(births_mye_path))
   deaths <- data.frame(readRDS(deaths_mye_path))
   target_curves <- data.frame(readRDS(target_curves_filepath)) %>% select(-year)
-  
+
   validate_scaled_mortality_curve_inputs(population, births, deaths, target_curves, last_data_year, n_years_to_avg,
                   avg_or_trend, data_col, output_col)
-  
-  population <- deaths_denominator(population, births)
-  
+
+  #Deaths denominator
+  population <- population %>%
+    popmodules::popn_age_on(births=births) %>%
+    # TODO do we want to do this select here?  Could be confusing if we want to keep more cols
+    select(gss_code, year, sex, age, popn) %>%
+    arrange(gss_code, year, sex, age)
+
   # Calculate the total deaths per year for each geography and sex that the target mortality curve would would create from population
   # Compare to the total deaths per year for each geography and sex in the actual deaths
   # *scaling* tells you what you would need to scale each geog and sex of the target curve by to get the same total deaths as the actuals
   # This is done because we prefer the ONS age structure for the first projection year to the previous actuals age structures
-  
+
   scaling_backseries <- left_join(population, target_curves, by = c("gss_code", "age", "sex")) %>%
     mutate(curve_count = rate * popn) %>%
     left_join(deaths, by = c("gss_code", "age", "sex", "year")) %>%
@@ -60,62 +65,35 @@ scaled_mortality_curve <- function(popn_mye_path, births_mye_path, deaths_mye_pa
                             0,
                             actual / curve_count)) %>%
     select(gss_code, year, sex, scaling)
-  
+  #browser()
   if(avg_or_trend == "trend"){
     averaged_scaling_factors <- calculate_rate_by_regression(scaling_backseries, n_years_regression = n_years_to_avg, last_data_year, data_col="scaling")
   }
-  
+
   if(avg_or_trend == "average"){
     averaged_scaling_factors <- calculate_mean_from_backseries(scaling_backseries, n_years_to_avg, last_data_year, data_col="scaling")
   }
-  
+
   validate_population(averaged_scaling_factors, col_aggregation = c("year", "gss_code", "sex"), col_data = "scaling")
-  
+
   jump_off_rates <- target_curves %>%
     left_join(averaged_scaling_factors, by = c("gss_code", "sex")) %>%
     mutate(jump_off_rate = scaling * rate) %>%
     select(gss_code, year, sex, age, jump_off_rate) %>%
     rename(!!output_col := jump_off_rate)
-  
+
   return(jump_off_rates)
-  
+
   # TODO should there be a check here that jump_off_rates values are reasonable?
-  
-}
 
-#--------------------------------------------------------------------
-
-#Function to create denominators for initial rate calculation
-# TODO move the code below into the main function for readability
-deaths_denominator <- function(population, births){
-  
-  assert_that(is.data.frame(population),
-              msg="deaths_denominator expects population to be a dataframe")
-  assert_that(is.data.frame(births),
-              msg="deaths_denominator expects births to be a dataframe")
-  
-  #TODO replace this with a general age_on function which adds in the births as well
-  #Deaths denominator
-  population <- population %>%
-    popmodules::popn_age_on(births=births) %>%
-    #TODO do we need the filter step below or should age_on do it?
-    filter(year != max(year)) %>%
-    # TODO do we want to do this select here?  Could be confusing if we want to keep more cols
-    select(gss_code, year, sex, age, popn) %>%
-    arrange(gss_code, year, sex, age)
-  
-  return(population)
-  
 }
 
 #-----------------------------------------------
 
-
-
 # Function to check that the input to scaled_mortality_curves is all legal
 validate_scaled_mortality_curve_inputs <- function(population, births, deaths, target_curves, last_data_year, n_years_to_avg,
                                                 avg_or_trend, data_col, output_col){
-  
+
   # test input parameters are of the correct type
   assert_that(is.data.frame(population),
               msg="scaled_mortality_curve expects that population is a data frame")
@@ -137,29 +115,29 @@ validate_scaled_mortality_curve_inputs <- function(population, births, deaths, t
               msg="scaled_mortality_curve expects that output_col is a character")
   assert_that(data_col %in% names(deaths),
               msg="scaled_mortality_curve expects that data_col is a column in deaths dataframe")
-  
+
   validate_population(population, col_aggregation = c("gss_code", "year", "sex", "age"), col_data = "popn")
   validate_population(births, col_aggregation = c("gss_code", "year", "sex", "age"), col_data = "births")
   validate_population(deaths, col_aggregation = c("gss_code", "year", "sex", "age"), col_data = "deaths")
   validate_population(target_curves, col_aggregation = c("gss_code", "age", "sex"), col_data = "rate")
-  
+
   invisible(TRUE)
-  
+
   }
 
 #----------------------------------------------
 
 validate_scaled_mortality_curve_filetype <- function(path_1, path_2, path_3, path_4) {
-  
+
   for(filepath in c(path_1, path_2, path_3, path_4)){
-    
+
     file_ext <- tolower(strsplit(basename(filepath), split="\\.")[[1]][[2]])
-    
+
     assertthat::assert_that(file_ext == "rds",
                             msg = paste(filepath,": file must be a .rds file"))
   }
-  
+
   invisible(TRUE)
-  
+
 }
 
