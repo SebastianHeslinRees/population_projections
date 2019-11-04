f9d611d452775ad49b7bf9d74711c2be7533148f
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2019-11-04 15:37:55 +0000

    edit to 03_output.R

--- model_code/model_scripts/trend/03_output.R
+++ model_code/model_scripts/trend/03_output.R
@@ -6,9 +6,37 @@ output_projection <- function(projection, output_dir, timestamp) {
   
   if (!grepl("/$", output_dir)) output_dir <- paste0(output_dir, "/")
   
+  #RDS
   lapply(seq_along(projection), 
          function(i) saveRDS(projection[[i]], paste0(output_dir, names(projection)[[i]],timestamp, ".rds"), compress = "gzip")) %>%
     invisible()
   
+  #CSV
+  csv_dir <- paste0(output_dir,"csv/")
+  dir.create(csv_dir)
+  
+  make_csvs <- function(data, name_stub){
+    
+    data_col <- names(data)[ncol(data)]
+    cols <- setdiff(names(data), data_col)
+    
+    female <- filter(data, sex == "female")
+    male <- filter(data, sex == "male")
+    persons <- data %>%
+      mutate(sex = "persons") %>%
+      rename(value = !!data_col) %>%
+      group_by_at(cols) %>%
+      summarise(!!data_col := sum(value)) %>%
+      ungroup()
+    
+    data.table::fwrite(female, paste0(name_stub,"_female.csv"))
+    data.table::fwrite(male, paste0(name_stub,"_male.csv"))
+    data.table::fwrite(persons, paste0(name_stub,"_persons.csv"))
+    
+  }
+  
+  lapply(seq_along(projection), 
+         function(i) make_csvs(projection[[i]], paste0(csv_dir, names(projection)[[i]]))) %>%
+    invisible()
+  
 }
-
