fb217da23a0a39fc638c97d9b61706ea0a8806f6
wiltonkiss <wil_tonkiss@yahoo.co.uk>
2019-11-12 15:29:34 +0000

    configs

--- documentation/templates/trend_snpp_2016_config.R
+++ documentation/templates/trend_snpp_2016_config.R
@@ -26,7 +26,7 @@ system.time({
   mortality_trajectory_filepath <- "input_data/mortality/npp_mortality_trend.rds"
   mortality_npp_variant <- "2016_principal"
   
-  fertility_years_to_avg <- 1
+  fertility_years_to_avg <- 5
   fertility_avg_or_trend <- "average"
   fertility_last_data_year <- 2016
   fertility_curve_filepath <- "input_data/fertility/ons_asfr_curves.rds"
