a588a480d9b0bac29e3077a9223e48fa5baac7ab
wiltonkiss <wil_tonkiss@yahoo.co.uk>
2019-12-05 12:42:21 +0000

    wip

--- model_code/model_scripts/housing_led/housing_led_control.R
+++ model_code/model_scripts/housing_led/housing_led_control.R
@@ -1,36 +1,52 @@
+#config
+constraint_data_fns <- get_data_from_file(list(
+  birth_constraint = birth_constraint_path,
+  death_constraint = death_constraint_path,
+  international_out_constraint = int_out_constraint_path,
+  population_constraint = popn_constraint_path))
+
+component_rates_fns <- get_data_from_file(list(
+  fertility_rates = fert_rates_path,
+  mortality_probabilities = mort_probs_path,
+  int_out_flows_rates = int_out_path,
+  int_in_flows = int_in_path,
+  domestic_rates = domestc_rates_path))
+
+#control
+housing_led_control <- function(config_list){}
+
+expected_config <- c(constraint_data_fns,
+                     component_rates_fns,
+                     communal_est_path,
+                     dev_trajectory_path,
+                     ahs_trajectory_path,
+                     dwelling_ratio_path,
+                     first_proj_yr)
+
 #The control will mostly be for reading-in data
 #And managing the return from the core loop
 
 #For constraining
-#Need to adapt get_constraints_from_file to do this
-#Pass to core as a named list
-birth_constraint
-death_constraint
-international_out_constraint
-population_constraint
+constraints <- evaluate_fns_list(config_list$constraint_data_fns)
+
+#component rates
+component_rates <- evaluate_fns_list(config_list$component_rates_fns)
+
+final_proj_yr <- max(constraints$population_constraint$year)
 
 #other data
 #use get_component
-communal_establishment_population
-average_household_size
-development_trajectory
-dwelling2household_ratio
-
-fertility_rates
-mortality_probabilities
-fertility_rates
-mortality_rates
-int_out_flows_rates
-int_in_flows
-domestic_rates
+communal_establishment_population <- get_component(config_list$communal_est_path, max_yr = final_proj_yr)
+average_household_size <- get_component(config_list$ahs_trajectory_path, max_yr = final_proj_yr)
+development_trajectory <- get_component(config_list$dev_trajectory_path, max_yr = final_proj_yr)
+dwelling2household_ratio <- readRDS(config_list$dwelling_ratio_path) %>% as.data.frame()
 
 #For trend model
-int_out_method #could this be detected from the input?
-constraints = NULL
+int_out_method #could/should this be detected from the input?
+npp_constraints = NULL
 upc = NULL
 
 #general
-projection_year
 offset_or_ratio_method
 
 #this added by BC on 10/05/2019.  Previously file only contained single set of ratios - these could be valid for only the DCLG or ONS model.
@@ -75,22 +91,22 @@ if(use_offset_method){
   
 }
 
-for(projection_year in 2019:2023){
+for(projection_year in first_proj_yr:final_proj_yr){
   
-  curr_yr_fertility <- filter(fertility_rates, year == projection_year)
-  curr_yr_mortality <- filter(mortality_rates, year == projection_year)
-  curr_yr_int_out <- filter(int_out_flows_rates, year == projection_year)
-  curr_yr_int_in_flows <- int_in_flows %>% filter(year == projection_year)
+  curr_yr_fertility <- filter(component_rates$fert_rates, year == projection_year)
+  curr_yr_mortality <- filter(component_rates$mort_probs, year == projection_year)
+  curr_yr_int_out <- filter(component_rates$int_out, year == projection_year)
+  curr_yr_int_in_flows <- component_rates$int_in %>% filter(year == projection_year)
   
   projection[[projection_year]] <- housing_led_core(start_population = curr_yr_popn, 
                                                     fertility_rates = curr_yr_fertility, 
                                                     mortality_rates = curr_yr_mortality,
                                                     int_out_flows_rates = curr_yr_int_out,
                                                     int_in_flows = curr_yr_int_in_flows,
-                                                    domestic_rates = domestic_rates,
+                                                    domestic_rates = component_rates$domestic_rates,
                                                     int_out_method = config_list$int_out_method,
                                                     npp_constraints = NULL, upc = NULL,
-                                                    borough_constraints,
+                                                    constraints = constraints,
                                                     communal_establishment_population,
                                                     average_household_size,
                                                     development_trajectory,
--- model_code/model_scripts/housing_led/housing_led_core.R
+++ model_code/model_scripts/housing_led/housing_led_core.R
@@ -1,67 +1,121 @@
-housing_led_core <- function(start_population = curr_yr_popn,
-                             fertility_rates = curr_yr_fertility,
-                             mortality_rates = curr_yr_mortality,
-                             int_out_flows_rates = curr_yr_int_out,
-                             int_in_flows = curr_yr_int_in_flows,
-                             domestic_rates = domestic_rates,
-                             int_out_method = config_list$int_out_method,
+housing_led_core <- function(start_population,
+                             fertility_rates,
+                             mortality_rates,
+                             int_out_flows_rates,
+                             int_in_flows,
+                             domestic_rates,
+                             int_out_method,
                              npp_constraints = NULL, upc = NULL,
-                             borough_constraints,
+                             constraints,
                              communal_establishment_population,
                              average_household_size,
                              development_trajectory,
                              dwelling2household_ratio,
                              projection_year = projection_year){
-
+  
   #1. Run the trend model for one year####
-
+  
   trend_population <- trend_core(start_population,
                                  fertility_rates, mortality_rates,
                                  int_out_flows_rates, int_in_flows,
                                  domestic_rates,
                                  int_out_method,
                                  constraints = npp_constraints,
-                                 upc = NULL,
+                                 upc = upc,
                                  projection_year)
-
-
+  
+  
   #2. Calculate household population
-  #There might be something in the household model function that does this
-
+  #There is something in the ons household model functions that does this
+  #Is it worth making this a functions - its so simple
+  #group this here or when it gets read in
+  ce <- dtplyr::lazy_dt(communal_establishment_population) %>%
+    group_by(gss_code, year) %>%
+    summarise(communal_est_popn = sum(communal_establishment_population)) %>%
+    data.frame()
+  
+  household_population <- dtplyr::lazy_dt(start_population) %>%
+    group_by(gss_code, year) %>%
+    summarise(popn = sum(popn)) %>%
+    left_join(ce, by=c("gss_code","year")) %>%
+    mutate(household_popn = popn - communal_est_popn) %>%
+    as.data.frame()
+  .check_negative_populations("houshold_popn") %>%
+    select(-popn, -communal_est_popn)
+  
   #3. Calculate trend AHS
   #Population divided by households
-
+  curr_yr_trend_ahs <- left_join(household_population, households, by=c("year","gss_code")) %>%
+    mutate(trend_ahs = household_popn/households) %>%
+    selecy(-household_popn, -households)
+  
+  #Somethings that sets cap here
+  
   #4. AHS Decision Tree
   #This will be new
-
+  
   #5. Target population
   #Probably apply_rate_to_population
-
+  target_adults <- apply_rate_to_population(households, ahs,
+                                            col_popn = "households",
+                                            col_rate = "ahs",
+                                            col_out = "target_popn",
+                                            col_aggregation = c("year","gss_code"))
+  
   #6. Constrain births, deaths & international
   #Constrain functions should work here - might need some additional functionality
-
+  births <- trend_projection["briths"] %>%
+    filter(gss_code %in% hma$gss_code) %>%
+    left_join(hma, by="gss_code") %>%
+    group_by(hma) %>%
+    summarise(births = sum(births)) %>%
+    ungroup() %>%
+    constrain_component(constraints,
+                        col_aggregation = c("year","hma"),
+                        col_popn = "births",
+                        col_constraint = "births")
+  
+  constrain_to_hma <- function(population, constrain, hma_list,
+                               col_popn, col_constraint,
+                               col_aggregation = c("hma","year")){
+    
+    
+    trend_projection["briths"] %>%
+      filter(gss_code %in% hma$gss_code) %>%
+      left_join(hma, by="gss_code") %>%
+      group_by(hma) %>%
+      summarise(births = sum(births)) %>%
+      ungroup() %>%
+      constrain_component(constraints,
+                          col_aggregation = c("year","hma"),
+                          col_popn = "births",
+                          col_constraint = "births")
+    
+  }
+  
+  
   #7. Add components from step 6 to domestic from step 1 & start population
   #Perhaps there's a need for a build_population_from_components function - we do something similar
   #at the end of the trend core at in step 10 here as well
-
+  
   #8. Compare population from step 7 to target from step 5. Difference = domestic adjustment
   #9. Adjust domestic
   #Is this a function to do both steps in one - ie identify adjustment and apply it
-
+  
   #10. Add components from step 6 to domestic from step 9 & start population
   #see 7
-
+  
   #11. Constrain total population
-
+  
   #12. Compare population from step 11 to population from step 10. Difference = domestic adjustment
   #13. Adjust domestic
   #see 8 & 9
-
+  
   return()
-
+  
   #Things we want to return:
   # final pop and components
   # ahs used
   # report on the ahs choice made
-
+  
 }
