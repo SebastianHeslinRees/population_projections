8651d65ddad3c4016ae59a3786679905d86ecae5
wiltonkiss <wil_tonkiss@yahoo.co.uk>
2019-12-10 15:41:01 +0000

    Update model_code/popmodules/R/get_data_from_file.R
    Co-Authored-By: ChrisF-GLA <48210749+ChrisF-GLA@users.noreply.github.com>

--- model_code/popmodules/R/get_data_from_file.R
+++ model_code/popmodules/R/get_data_from_file.R
@@ -12,7 +12,7 @@
 #' a <- list(cross_border_in_constraint = "input_data/constraints/npp_2016_cross_border_in_constraint.rds",
 #' cross_border_out_constraint = "input_data/constraints/npp_2016_cross_border_out_constraint.rds")
 #'
-#' g <- get_data_from_file_2(args = a)
+#' g <- get_data_from_file_2(files = a)
 
 get_data_from_file <- function(files) {
 
