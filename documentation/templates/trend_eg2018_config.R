0f86ae72eba02e84553df73da9b9dc32b9dfffb4
wiltonkiss <wil_tonkiss@yahoo.co.uk>
2019-12-11 14:19:23 +0000

    logging function

--- documentation/templates/trend_eg2018_config.R
+++ documentation/templates/trend_eg2018_config.R
@@ -213,4 +213,5 @@ file.copy(this_file, paste0(copy_dir, "/config_list_", config_list$timestamp, ".
 # Run the model
 source("model_code/model_scripts/trend/00_control.R")
 projection <- run_trend_model(config_list)
+log_warnings(paste0(copy_dir, "/warnings_", config_list$timestamp, ".txt"))
 
