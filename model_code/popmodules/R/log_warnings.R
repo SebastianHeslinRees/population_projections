eb6cf1b6f8785d11bb8f7cc99fea2760eb0722cf
wiltonkiss <wil_tonkiss@yahoo.co.uk>
2019-12-13 14:39:07 +0000

    tidy-up warnings logging

--- model_code/popmodules/R/log_warnings.R
+++ model_code/popmodules/R/log_warnings.R
@@ -1,25 +1,24 @@
 #' Write warnings to a text file
 #' 
 #' @param file_path A string giving the directory path and the name
-#' of the file to save..
+#' of the file to save.
 #'
 #' @export
 #' 
 log_warnings <- function(file_path){
   
   z <- attr(warnings(), "names")
-  n <- data.frame()
-  
+ 
+  n <- as.character()
   for(m in seq(z)){
-    n[m,1] <- z[[m]]
+    n[m] <- paste0(m, ": ", z[[m]], "\n")
   }
   
-  if(nrow(n) >= 50){
-    n <- data.frame(V1 = "More than 50 warnings were recorded. Showing the first 50 only.") %>%
-      rbind(n)
+  if(length(n) >= 5){
+    n <- c("More than 50 warnings were triggered. Showing the first 50 only.\n", n)
   }
   
-  data.table::fwrite(n, file_path, col.names=FALSE)
+  cat(as.character(n), sep = "\n", file = file_path, append = FALSE)
 
 }
 
