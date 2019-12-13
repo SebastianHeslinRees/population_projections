883138c7dd5dc499b4d033fff335061d679c9831
wiltonkiss <wil_tonkiss@yahoo.co.uk>
2019-12-13 14:42:20 +0000

    Update model_code/popmodules/R/log_warnings.R

--- model_code/popmodules/R/log_warnings.R
+++ model_code/popmodules/R/log_warnings.R
@@ -14,11 +14,10 @@ log_warnings <- function(file_path){
     n[m] <- paste0(m, ": ", z[[m]], "\n")
   }
   
-  if(length(n) >= 5){
+  if(length(n) >= 50){
     n <- c("More than 50 warnings were triggered. Showing the first 50 only.\n", n)
   }
   
   cat(as.character(n), sep = "\n", file = file_path, append = FALSE)
 
 }
-
