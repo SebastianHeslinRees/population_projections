4788ccad8caff4b99d698673c05421e048d199b9
Christopher Fairless <Christopher.Fairless@london.gov.uk>
2020-02-05 11:27:00 +0000

    Fix duplication from last merge

--- model_code/popmodules/R/calculate_scaling_factors.R
+++ model_code/popmodules/R/calculate_scaling_factors.R
@@ -45,7 +45,6 @@ calculate_scaling_factors <- function(popn,
   # Reformat col_aggregation to a named vector mapping between popn columns and constraint columns
   col_aggregation <- .convert_to_named_vector(col_aggregation)
 
-  # constraint will almost always be aggregated to the right levels, but it doesn't hurt to make sure
   if(anyDuplicated(data.table::as.data.table(constraint[col_aggregation]))) {
     #warning(paste("calculate_scaling_factors is aggregating the input constraints up to the requested levels.",
     #              "\nAlso, feel free to delete this warning in the source code if we're getting it too often, I'm just curious."))
@@ -70,16 +69,6 @@ calculate_scaling_factors <- function(popn,
 
   popn <- rename(popn, !!sym(col_popn_new) := !!sym(col_popn))
   constraint <- rename(constraint, !!sym(col_constraint_new) := !!sym(col_constraint))
-  
-   if(anyDuplicated(data.table::as.data.table(constraint[col_aggregation]))) {
-  #   warning(paste("calculate_scaling_factors is aggregating the input constraints up to the requested levels.",
-  #                 "\nAlso, feel free to delete this warning in the source code if we're getting it too often, I'm just curious."))
-     constraint <- lazy_dt(constraint) %>%
-       group_by_at(unname(col_aggregation)) %>%
-       summarise(!!sym(col_constraint_new) := sum(!!sym(col_constraint_new))) %>%
-       ungroup() %>%
-       data.frame()
-   }
 
   scaling <- popn %>%
     mutate(do_scale__ = !!enquo(rows_to_constrain)) %>%
