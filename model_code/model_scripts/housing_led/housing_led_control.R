10713d261535b2d4528fc6e36045096903118d1e
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-02-07 11:43:19 +0000

    validate control-created variables and data

--- model_code/model_scripts/housing_led/housing_led_control.R
+++ model_code/model_scripts/housing_led/housing_led_control.R
@@ -1,7 +1,7 @@
 run_housing_led_model <- function(config_list){
   
   message("Running borough model")
-
+  
   #validate
   expected_config <- c("projection_name",
                        "communal_est_file",
@@ -19,7 +19,7 @@ run_housing_led_model <- function(config_list){
                        "output_dir")
   
   if(!identical(sort(names(config_list)),  sort(expected_config))) stop("configuration list is not as expected")
-    
+  
   create_constraints <- function(dfs, col_aggregation=c("year","gss_code")){
     
     for(i in seq(dfs)){
@@ -35,10 +35,10 @@ run_housing_led_model <- function(config_list){
     
     return(dfs)
   }
-
+  
   external_trend_households_path <- paste0(config_list$external_trend_path,"households_",config_list$external_trend_datestamp,"/",config_list$trend_households_file)
   external_communal_est_path <- paste0(config_list$external_trend_path,"households_",config_list$external_trend_datestamp,"/",config_list$communal_est_file)
-  
+  browser()
   #component rates
   component_rates <- get_data_from_file(
     list(fertility_rates = paste0(config_list$external_trend_path,"fertility_rates_",config_list$external_trend_datestamp,".rds"),
@@ -67,11 +67,7 @@ run_housing_led_model <- function(config_list){
     group_by_at(c("year","hma","sex","age")) %>%
     summarise(popn = sum(popn)) %>%
     as.data.frame()
-  
-  #check
-  assertthat::assert_that(config_list$final_proj_yr <= max(hma_constraint$year))
 
-  
   #other data
   communal_establishment_population <- readRDS(external_communal_est_path) %>%
     dtplyr::lazy_dt() %>%
@@ -81,7 +77,7 @@ run_housing_led_model <- function(config_list){
   
   external_ahs <- readRDS(config_list$external_ahs_trajectory_path) %>% as.data.frame()
   development_trajectory <- readRDS(config_list$dev_trajectory_path) %>% project_forward_flat(2050)
-
+  
   #housing trajectory
   external_trend_households <- readRDS(external_trend_households_path) %>%
     filter(year <= config_list$ldd_max_yr)%>%
@@ -89,9 +85,6 @@ run_housing_led_model <- function(config_list){
     group_by(gss_code, year) %>%
     summarise(households = sum(households)) %>%
     as.data.frame()
-  
-  #TODO I need to decide what we're pre-processing and what we're doing in the control
-  #TODO This is missing census data for the base stock so the numbers make no sense
   
   #census stock in 2011 + LDD development upto 2019
   ldd_backseries <- readRDS(config_list$ldd_backseries_path)%>%
@@ -101,7 +94,7 @@ run_housing_led_model <- function(config_list){
   additional_dwellings <- ldd_backseries %>%
     rbind(filter(development_trajectory, year > config_list$ldd_max_yr)) %>%
     arrange(gss_code, year)
-    
+  
   dwelling_trajectory <- additional_dwellings %>%
     group_by(gss_code) %>%
     mutate(dwellings = cumsum(units)) %>%
@@ -110,23 +103,23 @@ run_housing_led_model <- function(config_list){
     arrange(gss_code, year) 
   
   dwelling2household_ratio_adjusted <- filter(external_trend_households,
-                                     year <= config_list$ldd_max_yr,
-                                     year %in% dwelling_trajectory$year,
-                                     gss_code %in% dwelling_trajectory$gss_code) %>%
+                                              year <= config_list$ldd_max_yr,
+                                              year %in% dwelling_trajectory$year,
+                                              gss_code %in% dwelling_trajectory$gss_code) %>%
     left_join(dwelling_trajectory, by=c("gss_code","year")) %>%
     mutate(dw2hh_ratio = households/dwellings) %>%
     select(year, gss_code, dw2hh_ratio) %>%
     project_forward_flat(config_list$final_proj_yr)
   
   dwelling2household_ratio_static <- filter(external_trend_households,
-                                     year == 2011,
-                                     gss_code %in% dwelling_trajectory$gss_code) %>%
+                                            year == 2011,
+                                            gss_code %in% dwelling_trajectory$gss_code) %>%
     left_join(dwelling_trajectory, by=c("gss_code","year")) %>%
     mutate(dw2hh_ratio = households/dwellings) %>%
     select(year, gss_code, dw2hh_ratio) %>%
     project_forward_flat(config_list$final_proj_yr)
   
-
+  
   #development_trajectories
   household_trajectory_static <- dwelling_trajectory %>% 
     left_join(dwelling2household_ratio_static, by=c("year","gss_code")) %>%
@@ -165,6 +158,16 @@ run_housing_led_model <- function(config_list){
   projection <- list()
   trend_projection <- list()
   
+  
+  #check
+  validate_housing_led_control_variables(first_proj_yr, final_proj_yr,
+                                         hma_constraint, component_rates,
+                                         component_constraints,
+                                         communal_establishment_population,
+                                         external_ahs,
+                                         external_trend_households,
+                                         dwelling_trajectory)
+  
   for(projection_year in first_proj_yr:final_proj_yr){
     
     curr_yr_fertility <- filter(component_rates$fertility_rates, year == projection_year)
@@ -175,17 +178,17 @@ run_housing_led_model <- function(config_list){
     curr_yr_households_static <- filter(household_trajectory_static, year == projection_year)
     curr_yr_households_adjusted <- filter(household_trajectory_adjusted, year == projection_year)
     curr_yr_hma_constraint <- filter(hma_constraint, year == projection_year)
-    
+    browser()
     trend_projection[[projection_year]] <- trend_core(start_population = curr_yr_popn,
-                                   fertility_rates = curr_yr_fertility, 
-                                   mortality_rates = curr_yr_mortality,
-                                   int_out_flows_rates = curr_yr_int_out,
-                                   int_in_flows = curr_yr_int_in_flows,
-                                   domestic_rates = component_rates$domestic_rates,
-                                   int_out_method = int_out_method,
-                                   constraints = npp_constraints,
-                                   upc = upc,
-                                   projection_year)
+                                                      fertility_rates = curr_yr_fertility, 
+                                                      mortality_rates = curr_yr_mortality,
+                                                      int_out_flows_rates = curr_yr_int_out,
+                                                      int_in_flows = curr_yr_int_in_flows,
+                                                      domestic_rates = component_rates$domestic_rates,
+                                                      int_out_method = int_out_method,
+                                                      constraints = npp_constraints,
+                                                      upc = upc,
+                                                      projection_year)
     
     projection[[projection_year]] <- housing_led_core(start_population = curr_yr_popn, 
                                                       trend_projection = trend_projection[[projection_year]],
@@ -220,4 +223,36 @@ run_housing_led_model <- function(config_list){
                                 dwelling_trajectory,
                                 household_trajectory_static,
                                 first_proj_yr)
+}
+
+validate_housing_led_control_variables <- function(first_proj_yr, final_proj_yr,
+                                                   hma_constraint, component_rates,
+                                                   component_constraints,
+                                                   communal_establishment_population,
+                                                   external_ahs,
+                                                   external_trend_households,
+                                                   dwelling_trajectory){
+  
+  assertthat::assert_that(min(hma_constraint$year) <= final_proj_yr)
+  assertthat::assert_that(min(component_rates[['fertility_rates']]$year) <= first_proj_yr)
+  assertthat::assert_that(min(component_rates[['fertility_rates']]$year) <= first_proj_yr)
+  assertthat::assert_that(min(component_constraints[['birth_constraint']]$year) <= first_proj_yr)
+  assertthat::assert_that(min(component_constraints[['death_constraint']]$year) <= first_proj_yr)
+  assertthat::assert_that(min(component_constraints[['international_out_constraint']]$year) <= first_proj_yr)
+  assertthat::assert_that(min(communal_establishment_population$year) <= first_proj_yr)
+  assertthat::assert_that(min(external_ahs$year) <= first_proj_yr)
+  assertthat::assert_that(min(external_trend_households$year) <= first_proj_yr)
+  assertthat::assert_that(min(dwelling_trajectory$year) <= first_proj_yr)
+  
+  assertthat::assert_that(max(hma_constraint$year) >= final_proj_yr)
+  assertthat::assert_that(max(component_rates[['fertility_rates']]$year) >= final_proj_year)
+  assertthat::assert_that(max(component_rates[['fertility_rates']]$year) >= final_proj_year)
+  assertthat::assert_that(max(component_constraints[['birth_constraint']]$year) >= final_proj_year)
+  assertthat::assert_that(max(component_constraints[['death_constraint']]$year) >= final_proj_year)
+  assertthat::assert_that(max(component_constraints[['international_out_constraint']]$year) >= final_proj_year)
+  assertthat::assert_that(max(communal_establishment_population$year) >= final_proj_year)
+  assertthat::assert_that(max(external_ahs$year) >= final_proj_year)
+  assertthat::assert_that(max(external_trend_households$year) >= final_proj_year)
+  assertthat::assert_that(max(dwelling_trajectory$year) >= final_proj_year)
+  
 }
