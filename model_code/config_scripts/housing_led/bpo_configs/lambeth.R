9406b814bbbf626723d9b3c81990d15a12bd7d5c
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-02-17 13:34:19 +0000

    more configs

--- model_code/config_scripts/housing_led/bpo_configs/lambeth.R
+++ model_code/config_scripts/housing_led/bpo_configs/lambeth.R
@@ -1,8 +1,8 @@
 source('model_code/model_scripts/housing_led/run_bpo_projection.R')
 
 for(scenario in c("high","medium","low")){
-  bpo_name <- run_bpo_projection(bpo_name = "merton_low",
-                                 shlaa_first_year = 2042,
+  bpo_name <- run_bpo_projection(bpo_name = "lambeth_shlaa",
+                                 shlaa_first_year = 2019,
                                  final_proj_yr = 2050,
                                  migration_scenario = scenario)
 }
