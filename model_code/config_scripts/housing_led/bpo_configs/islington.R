a5c1ecc4b4c749c3a22ebc0b94469d4bbd43bdd2
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-02-20 15:07:41 +0000

    new config

---
+++ model_code/config_scripts/housing_led/bpo_configs/islington.R
@@ -0,0 +1,9 @@
+source('model_code/model_scripts/housing_led/run_bpo_projection.R')
+
+for(scenario in c("high","medium","low")){
+
+  bpo_name <- run_bpo_projection(bpo_name = "islington",
+                                 shlaa_first_year = 2036,
+                                 final_proj_yr = 2050,
+                                 migration_scenario = scenario)
+}
