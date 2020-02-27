fafbd012d471454276a9ac36c34cbdbbb91d865f
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-02-27 09:56:25 +0000

    camden & bromley

--- model_code/model_scripts/housing_led/run_bpo_projection.R
+++ model_code/model_scripts/housing_led/run_bpo_projection.R
@@ -43,6 +43,11 @@ run_bpo_projection <- function(bpo_name,
       domestic_transition_yr = 2024,
       domestic_initial_rate_path = "outputs/trend/2018/2018_short_19-11-13_2205/domestic_rates.rds",
       domestic_long_term_rate_path = "input_data/migration/low_domestic_migration_rates_(2009_2012).rds")
+  }else if(migration_scenario == "other"){
+    housing_led_params <- list(
+      domestic_transition_yr = NULL,
+      domestic_initial_rate_path = "input_data/migration/low_domestic_migration_rates_(2009_2012).rds",
+      domestic_long_term_rate_path = NULL)
   } else {stop("migration scenario must be low, medium or high") }
 
   #trajectory
