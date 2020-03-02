5fb7c02293c657e0ea78d98a904b13aba39cc258
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-03-02 16:39:15 +0000

    ceate package hlm

--- model_code/housingledmodel/R/run_bpo_projection.R
+++ model_code/housingledmodel/R/run_bpo_projection.R
@@ -1,20 +1,20 @@
 #' Run the borough and ward housing-led models using a BPO dwelling trajectory
-#' 
+#'
 #' A wrapper for create_bpo_trajectory and run_borough_and_ward
 #'
 #' @param bpo_name String. The name of the dwelling trajectory csv saved in the folder
 #'   \code{bpo_dir} folder. The output projection will also have this name.
 #' @param shlaa_first_yr Numeric. The first year to use shlaa development data.
-#'   Effectively the final year of the supplied trajectory plus 1. \code{Default 2042}.  
+#'   Effectively the final year of the supplied trajectory plus 1. \code{Default 2042}.
 #' @param first_proj_yr Numeric. The first projection year \code{default 2019}
 #' @param final_proj_yr Numeric. The final projection year \code{default 2050}
 #' @param dev_first_yr Numeric. The first year for which development data is provided. \code{Default 2019}.
 #' @param ldd_last_yr Numeric. The final year of the LDD dwellings backsries. \code{Default 2018}.
 #' @param bpo_dir String. The folder containing the dwelling trajectory csv
-#' @migration_sceanrio String. The domestic migration scenario \code{high}, \code{medium} or \code{low}.
+#' @param migration_sceanrio String. The domestic migration scenario \code{high}, \code{medium} or \code{low}.
 #'
 #' @import dplyr
-#' 
+#'
 #' @export
 
 
@@ -26,7 +26,7 @@ run_bpo_projection <- function(bpo_name,
                                ldd_final_yr = 2018,
                                bpo_dir = "Q:/Teams/D&PA/Demography/Projections/bpo_2018_based/",
                                migration_scenario){
-  
+
   #domestic migration
   if(migration_scenario == "high"){
     housing_led_params <- list(
@@ -59,12 +59,12 @@ run_bpo_projection <- function(bpo_name,
                                      shlaa_first_yr = shlaa_first_yr,
                                      dev_first_yr = dev_first_yr,
                                      ldd_final_yr = ldd_final_yr)
-  
+
   dev_trajectory_path <- paste0(bpo_dir,"rds/bpo_borough_trajectory_",bpo_name,".rds")
   small_area_dev_trajectory_path <- paste0(bpo_dir,"rds/bpo_ward_trajectory_",bpo_name,".rds")
   projection_name <- paste0(bpo_name,"_",migration_scenario,"_migration")
   housing_led_params[['constrain_projection']] <- FALSE
-  
+
   #projection
   bpo_projection <- run_borough_and_ward_projection(projection_name = projection_name,
                                                     dev_trajectory_path,
@@ -73,7 +73,7 @@ run_bpo_projection <- function(bpo_name,
                                                     final_proj_yr,
                                                     bpo = borough_gss,
                                                     housing_led_params = housing_led_params)
-  
+
   message(paste(bpo_name, "-",migration_scenario,"complete"))
-  
+
 }
