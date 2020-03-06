49b259f9edb9effd680e82fa94e11a6c10189e69
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-03-06 12:03:03 +0000

    config

--- model_code/config_scripts/housing_led/bpo_configs/city_of_london_decoupled.R
+++ model_code/config_scripts/housing_led/bpo_configs/city_of_london_decoupled.R
@@ -4,6 +4,7 @@ for(scenario in c("high","medium","low")){
   bpo_name <- run_bpo_projection(bpo_name = "city_of_london_decoupled",
                                  shlaa_first_yr = 2027,
                                  final_proj_yr = 2050,
+                                 first_proj_yr = 2012,
                                  migration_scenario = scenario,
                                  csv_name = "city_of_london")
 }
