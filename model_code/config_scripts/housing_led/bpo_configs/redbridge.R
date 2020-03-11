ea308001f38affd83232069962ef0cec81b20c03
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-03-11 13:16:26 +0000

    redbridge config

---
+++ model_code/config_scripts/housing_led/bpo_configs/redbridge.R
@@ -0,0 +1,9 @@
+source('model_code/model_scripts/housing_led/run_bpo_projection.R')
+
+for(scenario in c("high","medium","low")){
+  bpo_name <- run_bpo_projection(bpo_name = "redbridge",
+                                 shlaa_first_yr = 2031,
+                                 final_proj_yr = 2050,
+                                 dev_first_yr = 2020,
+                                 migration_scenario = scenario)
+}
