0364b968afb91220bb363dc8420e82b6d304366c
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-04-01 13:12:15 +0100

    camden config

--- model_code/config_scripts/housing_led/bpo_configs/camden.R
+++ model_code/config_scripts/housing_led/bpo_configs/camden.R
@@ -1,12 +1,13 @@
 source('model_code/model_scripts/housing_led/run_bpo_projection.R')
 
-for(scenario in c("high","medium","low")){
+#for(scenario in c("high","medium","low")){
   
-  bpo_name <- run_bpo_projection(bpo_name = "camden_trended_births",
-                                 shlaa_first_yr = 2032,
+  scenario <- "medium"
+  bpo_name <- run_bpo_projection(bpo_name = "camden",
+                                 shlaa_first_yr = 2033,
                                  final_proj_yr = 2050,
                                  migration_scenario = scenario,
-                                 csv_name = "camden",
-                                 housing_led_params = list(fertility_rates_path = "input_data/fertility/fertility_rates_inc_2019_in_london_5yr_trend.rds",
-                                                           additional_births_path = "input_data/fertility/births_2019.rds"))
-}
+                                 dev_first_yr = 2012,
+                                 ldd_final_yr = 2011,
+                                 fertility_scenario = "trend")
+#}
