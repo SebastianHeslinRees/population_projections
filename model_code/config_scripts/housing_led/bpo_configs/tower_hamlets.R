f22383a3ccc03b480eb8980f906453d350ef7d8c
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-04-14 15:40:29 +0100

    typo

--- model_code/config_scripts/housing_led/bpo_configs/tower_hamlets.R
+++ model_code/config_scripts/housing_led/bpo_configs/tower_hamlets.R
@@ -12,4 +12,4 @@ bpo_name <- run_bpo_projection(bpo_name = "tower_hamlets",
                                shlaa_first_yr = 2035,
                                final_proj_yr = 2050,
                                migration_scenario = "medium",
-                               fertility_sceanrio = "trend")
+                               fertility_scenario = "trend")
