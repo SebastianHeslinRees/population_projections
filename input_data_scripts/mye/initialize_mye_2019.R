20e5f0a9d8a8f26642c4fddaaeaa516123698369
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-05-12 13:31:36 +0100

    initialize script

---
+++ input_data_scripts/mye/initialize_mye_2019.R
@@ -0,0 +1,20 @@
+#ONS population
+source('input_data_scripts/mye/ons_popn_2019.R')
+
+#Births
+source('input_data_scripts/mye/births_2019.R')
+
+#Deaths
+source('input_data_scripts/mye/deaths_2019.R')
+
+#International (ONS & GLA)
+source('input_data_scripts/mye/international_2019.R')
+
+#Domestic (takes a minute)
+source('input_data_scripts/mye/domestic_2019.R')
+
+#GLA population
+source('input_data_scripts/mye/gla_popn_2019.R')
+
+#household model inputs
+source('input_data_scripts/households/household_model_inputs_(2020 geog).R')
