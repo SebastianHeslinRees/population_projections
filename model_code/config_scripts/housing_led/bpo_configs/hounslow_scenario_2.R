974d6ae7a83848191c1be6d0068ffbd2a172164a
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-05-13 15:53:21 +0100

    hounslow config

---
+++ model_code/config_scripts/housing_led/bpo_configs/hounslow_scenario_2.R
@@ -0,0 +1,17 @@
+source('model_code/model_scripts/housing_led/run_bpo_projection.R')
+
+for(scenario in c("high","medium","low")){
+  bpo_name <- run_bpo_projection(bpo_name = "hounslow_scenario_2",
+                                 dev_first_yr = 2020,
+                                 shlaa_first_yr = 2035,
+                                 final_proj_yr = 2050,
+                                 migration_scenario = scenario,
+                                 fertility_sceanrio = "average")
+}
+
+bpo_name <- run_bpo_projection(bpo_name = "hounslow_scenario_2",
+                               dev_first_yr = 2020,
+                               shlaa_first_yr = 2035,
+                               final_proj_yr = 2050,
+                               migration_scenario = scenario,
+                               fertility_sceanrio = "trend")
