014fa14224d6d6639574d0b3b966e8b3f2244cc9
Chris Fairless <Christopher.Fairless@london.gov.uk>
2020-05-20 09:25:08 +0100

    Edit some gss_recode years, fix deaths 2019 inputs
    - Some gss_recoding for 2020 data didn't specify the year
    - The deaths 2019 data read in the wrong year at one point

--- input_data_scripts/households/household_model_inputs_(2020 geog).R
+++ input_data_scripts/households/household_model_inputs_(2020 geog).R
@@ -1,3 +1,7 @@
+library(assertthat)
+library(dplyr)
+devtools::load_all("model_code/popmodules/")
+
 ons_stage1 <- readRDS("input_data/household_model/ons_household_representative_rates.rds")
 ons_stage2 <- readRDS("input_data/household_model/ons_headship_rates_2016.rds")
 communal_est <- readRDS("input_data/household_model/ons_communal_establishment_population.rds")
@@ -5,28 +9,32 @@ dclg_stage1 <- readRDS("input_data/household_model/dclg_stage1_data_2014.rds")
 dclg_stage2 <- readRDS("input_data/household_model/dclg_headship_rates_2014.rds")
 
 ons_stage1 <- recode_gss_codes(ons_stage1,
-                                 col_aggregation = c("gss_code","year","sex","age_group"),
-                                 col_geog="gss_code",
-                                 fun = list(mean))
+                               col_aggregation = c("gss_code","year","sex","age_group"),
+                               col_geog="gss_code",
+                               fun = list(mean),
+                               recode_to_year = 2020)
 ons_stage2 <- recode_gss_codes(ons_stage2,
-                                 col_aggregation = c("gss_code","year","household_type","age_group"),
-                                 col_geog="gss_code",
-                                 fun = list(mean))
-sum(is.na(ons_stage1))
-sum(is.na(ons_stage2))
+                               col_aggregation = c("gss_code","year","household_type","age_group"),
+                               col_geog="gss_code",
+                               fun = list(mean),
+                               recode_to_year = 2020)
+assert_that(!any(is.na(ons_stage1)))
+assert_that(!any(is.na(ons_stage2)))
 
 communal_est_pop <- communal_est %>%
   select(-ce_rate) %>%
   recode_gss_codes(col_aggregation = c("gss_code","age_group","sex","year"),
-                     col_geog="gss_code")
+                   col_geog="gss_code",
+                   recode_to_year = 2020)
 
 communal_est_rate <- communal_est %>% 
   select(-ce_pop) %>%
   recode_gss_codes(col_aggregation = c("gss_code","age_group","sex","year"),
-                     col_geog="gss_code",
-                     fun = list(mean))
+                   col_geog="gss_code",
+                   fun = list(mean),
+                   recode_to_year = 2020)
 
-sum(is.na(communal_est_pop))
+assert_that(!any(is.na(communal_est_pop)))
 
 communal_est <- left_join(communal_est_pop, communal_est_rate,
                           by = c("gss_code","age_group","sex","year"))
@@ -34,13 +42,15 @@ communal_est <- left_join(communal_est_pop, communal_est_rate,
 dclg_stage1_pops <- dclg_stage1 %>%
   select(-hh_rep_rates) %>%
   recode_gss_codes(col_aggregation = c("gss_code","year","sex","household_type","age_group"),
-                     col_geog="gss_code")
+                   col_geog="gss_code",
+                   recode_to_year = 2020)
 
 dclg_stage1_rates <- dclg_stage1 %>%
   select(-households, -household_population, -institutional_population, -total_population) %>%
   recode_gss_codes(col_aggregation = c("gss_code","year","sex","household_type","age_group"),
-                     col_geog="gss_code",
-                     fun=list(mean))
+                   col_geog="gss_code",
+                   fun=list(mean),
+                   recode_to_year = 2020)
 
 dclg_stage1 <- left_join(dclg_stage1_pops, dclg_stage1_rates,
                          by = c("gss_code","year","sex","household_type","age_group"))
@@ -48,8 +58,9 @@ dclg_stage1 <- left_join(dclg_stage1_pops, dclg_stage1_rates,
 
 dclg_stage2 <- dclg_stage2 %>%
   recode_gss_codes(col_aggregation = c("gss_code","year","household_type","age_group"),
-                     col_geog="gss_code",
-                     fun=list(mean))
+                   col_geog="gss_code",
+                   fun=list(mean),
+                   recode_to_year = 2020)
 
 saveRDS(ons_stage1,"input_data/household_model/ons_household_representative_rates_(2020_geog).rds")
 saveRDS(ons_stage2,"input_data/household_model/ons_headship_rates_2016_(2020_geog).rds")
