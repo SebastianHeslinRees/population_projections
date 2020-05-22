55e729ee3f49e8ec8c7958132401b3ce8cda5f53
Chris Fairless <Christopher.Fairless@london.gov.uk>
2020-05-22 09:33:23 +0100

    Fixes to the 2019-20 input creation
    - Changed domestic and gla_popn scripts to use the
    construct_population_from_components function. Doesn't change the
    outputs and wasn't reeeeally necessary
    
    - Edit how the domestic flows are re-aggregated with the recode gss
    function. The old method allowed for flows from a merged gss_code to
    itself
    
    - The domestic migration backseries contains regional aggregations, so
    added a call to filter_to_LAs() when they're read in in gla_popn_2019

--- input_data_scripts/mye/gla_popn_2019.R
+++ input_data_scripts/mye/gla_popn_2019.R
@@ -7,41 +7,33 @@ gla_popn <- readRDS("input_data/mye/2018/population_gla_2019-11-13.rds") %>%
   recode_gss_codes(col_aggregation = c("year","gss_code","sex","age"), recode_to_year = 2020)
 
 births <- readRDS("input_data/mye/2019/temp_births.rds") %>%
-  filter(year == 2019) %>%
-  rename(popn = births)
+  filter(year == 2019)
 
 deaths <- readRDS("input_data/mye/2019/temp_deaths.rds") %>%
-  filter(year == 2019) %>%
-  mutate(popn = deaths*-1) %>%
-  select(-deaths)
+  filter(year == 2019)
 
 gla_international_in <- readRDS("input_data/mye/2019/temp_gla_international_in.rds") %>%
-  filter(year == 2019) %>%
-  rename(popn = int_in)
+  filter(year == 2019)
 
 gla_international_out <- readRDS("input_data/mye/2019/temp_gla_international_out.rds") %>%
-  filter(year == 2019) %>%
-  mutate(popn = int_out*-1) %>%
-  select(-int_out)
+  filter(year == 2019)
 
 dom_in <- readRDS("input_data/domestic_migration/2019/temp_domestic_migration_in.rds") %>%
   filter(year == 2019) %>%
-  rename(popn = dom_in)
+  filter_to_LAs()
 
 dom_out <- readRDS("input_data/domestic_migration/2019/temp_domestic_migration_out.rds") %>%
   filter(year == 2019) %>%
-  mutate(popn = dom_out*-1) %>%
-  select(-dom_out)
+  filter_to_LAs()
 
 popn_2019 <- gla_popn %>%
   filter(year == 2018) %>%
-  rbind(births, deaths, gla_international_in, gla_international_out, dom_in, dom_out) %>%
-  filter(gss_code %in% unique(births$gss_code)) %>% 
-  group_by(gss_code, sex, age) %>%
-  summarise(popn = sum(popn)) %>%
-  as.data.frame() %>%
+  construct_popn_from_components(addition_data = list(births, gla_international_in, dom_in),
+                                 subtraction_data = list(deaths, gla_international_out, dom_out),
+                                 col_aggregation = c("gss_code", "age", "sex")) %>%
   mutate(year = 2019) %>%
   select(year, gss_code, sex, age, popn) %>%
+  arrange(year, gss_code, sex, age) %>%
   check_negative_values(data_col = "popn")
 
 gla_mye <- rbind(gla_popn, popn_2019)
@@ -51,7 +43,7 @@ validate_same_geog(births, gla_international_in)
 validate_same_geog(births, gla_international_out)
 validate_same_geog(births, deaths)
 validate_same_geog(gla_popn, popn_2019)
-sum(is.na(popn_2019))
+assert_that(!any(is.na(popn_2019)))
 
 saveRDS(gla_mye, "input_data/mye/2019/temp_gla_population.rds")
 
