cf83c0053d5be30e25956710facf3824a76ddfdc
Chris Fairless <Christopher.Fairless@london.gov.uk>
2020-06-11 20:06:34 +0100

    No need to specify popn and births for MYE flow calculations

--- input_data_scripts/migration/pre-calculate_international_in_flows.R
+++ input_data_scripts/migration/pre-calculate_international_in_flows.R
@@ -4,14 +4,12 @@ devtools::load_all('model_code/popmodules')
 output_loc <- "input_data/mye/"
 
 #2016 Flows
-popn_mye_path <- "input_data/mye/2016/population_gla_.rds"
-births_mye_path <-  "input_data/mye/2016/births_gla_.rds"
 int_in_mye_path <-  "input_data/mye/2016/international_in_gla_.rds"
 int_out_mye_path <- "input_data/mye/2016/international_out_gla_.rds"
 
 int_in_10yr_avg_2016 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path,
-  births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2016,
@@ -23,8 +21,8 @@ int_in_10yr_avg_2016 <- popmodules::calculate_mean_international_rates_or_flows(
   select(-year)
 
 int_in_5yr_avg_2016 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path,
-  births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2016,
@@ -36,14 +34,12 @@ int_in_5yr_avg_2016 <- popmodules::calculate_mean_international_rates_or_flows(
   select(-year)
 
 #2018 Flows
-popn_mye_path <- paste0("input_data/mye/2018/population_gla_2019-11-13.rds")
-births_mye_path <-  paste0("input_data/mye/2018/births_ons.rds")
 int_out_mye_path <-  paste0("input_data/mye/2018/international_out_gla_2019-11-13.rds")
 int_in_mye_path <-  paste0("input_data/mye/2018/international_in_gla_2019-11-13.rds")
 
 int_in_10yr_avg_2018 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path,
-  births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2018,
@@ -55,8 +51,8 @@ int_in_10yr_avg_2018 <- popmodules::calculate_mean_international_rates_or_flows(
   select(-year)
 
 int_in_15yr_avg_2018 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path,
-  births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2018,
@@ -68,7 +64,8 @@ int_in_15yr_avg_2018 <- popmodules::calculate_mean_international_rates_or_flows(
   select(-year)
 
 int_in_5yr_avg_2018 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path, births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2018,
@@ -82,16 +79,14 @@ int_in_5yr_avg_2018 <- popmodules::calculate_mean_international_rates_or_flows(
 #-------------------------------
 
 #2019 Flows
-popn_mye_path <- "input_data/mye/2019/temp_gla_population.rds"
-births_mye_path <- "input_data/mye/2019/temp_births.rds"
 int_out_mye_path <- "input_data/mye/2019/temp_gla_international_out.rds"
 int_in_mye_path <-  "input_data/mye/2019/temp_gla_international_in.rds"
 
 #10 year average
 
 int_in_10yr_avg_2019 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path,
-  births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2019,
@@ -103,8 +98,8 @@ int_in_10yr_avg_2019 <- popmodules::calculate_mean_international_rates_or_flows(
   select(-year)
 
 int_in_15yr_avg_2019 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path,
-  births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2019,
@@ -116,8 +111,8 @@ int_in_15yr_avg_2019 <- popmodules::calculate_mean_international_rates_or_flows(
   select(-year)
 
 int_in_5yr_avg_2019 <- popmodules::calculate_mean_international_rates_or_flows(
-  popn_mye_path = popn_mye_path,
-  births_mye_path = births_mye_path,
+  popn_mye_path = NULL,
+  births_mye_path = NULL,
   flow_or_rate = "flow",
   component_path = int_in_mye_path,
   last_data_year = 2019,
