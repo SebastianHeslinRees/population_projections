3b2e70ae47ed4737a822d46bb4d6f623b5dcc1b8
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-06-11 15:10:40 +0100

    remove test files and update test script so temp files are deleted

--- model_code/popmodules/tests/testthat/test-project_rates_npp.R
+++ model_code/popmodules/tests/testthat/test-project_rates_npp.R
@@ -1,8 +1,14 @@
+context("project_rates_npp")
+library(popmodules)
+library(testthat)
+library(dplyr)
+
+rate_trajectory_filepath <- "test_data/test_project_rates_npp_data.rds"
 
 trend <- expand.grid(sex = c("female","male"), age = 21:22, year = 2010:2020, variant = "2018_principal") %>%
   arrange(sex, age, variant) %>%
   mutate(change = 0.1 * (year - 2009))
-saveRDS(trend, "test_data/test_ptoject_rates_npp_data.rds")
+saveRDS(trend, rate_trajectory_filepath)
 
 jump_off <- expand.grid(year = 2012, gss_code = c("E01","E02"), sex = c("female","male"), age = 21:22, test_rate = 0.1)
 
@@ -24,8 +30,10 @@ rate_trajectory_filepath <- "test_data/test_ptoject_rates_npp_data.rds"
 x <- project_rates_npp(jump_off, rate_col="test_rate", rate_trajectory_filepath, first_proj_yr=2012, n_proj_yr=9, npp_var="2018_principal")
 
 testthat::test_that("project_rates_npp produces the expected output",
-  expect_equal(x, proj)
-)
+                    expect_equal(x, proj))
+
+file.remove(rate_trajectory_filepath)
+
 
 
 
