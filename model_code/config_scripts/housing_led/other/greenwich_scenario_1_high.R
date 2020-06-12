a62f5e16ca225b47d74306fc86e48ca44c752293
Chris Fairless <Christopher.Fairless@london.gov.uk>
2020-06-12 18:19:41 +0100

    Update greenwich scenarios with bpo parameter name change

--- model_code/config_scripts/housing_led/other/greenwich_scenario_1_high.R
+++ model_code/config_scripts/housing_led/other/greenwich_scenario_1_high.R
@@ -2,7 +2,7 @@ source('model_code/model_scripts/housing_led/run_bpo_projection.R')
 
 bpo_name <- run_bpo_projection(bpo_name = "greenwich_scenario_1",
                                shlaa_first_yr = 2035,
-                               final_proj_yr = 2050,
+                               last_proj_yr = 2050,
                                dev_first_yr = 2020,
                                migration_scenario = "high")
 
