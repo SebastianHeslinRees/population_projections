83008e63a4b21dbb9b2e6fbeb4ac1df507180643
Chris Fairless <Christopher.Fairless@london.gov.uk>
2020-06-18 13:16:42 +0100

    Copy shlaa data over during initialize.R

--- input_data_scripts/initialize_small_area.R
+++ input_data_scripts/initialize_small_area.R
@@ -27,6 +27,9 @@ source('input_data_scripts/small_area_data/msoa_communal_establishment_populatio
 source('input_data_scripts/small_area_data/msoa_adults_per_dwelling.R')
 source('input_data_scripts/small_area_data/msoa_migration_data.R')
 
+file.copy("Q:/Teams/D&PA/Data/housing_development/shlaa/shlaa_2016/October 2017 (v2)/Output/borough_shlaa_trajectory.rds",
+          "input_data/housing_led_model/borough_shlaa_trajectory.rds")
+
 #TESTS
 rm(list=ls())
 ward_to_district <- readRDS("input_data/lookup/2011_ward_to_district.rds")
