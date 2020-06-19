292cd604584471b0a74a9f95191eff676f1b5508
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-06-19 09:23:38 +0100

    housing-led configs

--- model_code/config_scripts/housing_led/housing_led_config_constrained.R
+++ model_code/config_scripts/housing_led/housing_led_config_constrained.R
@@ -1,4 +1,5 @@
 devtools::load_all("model_code/popmodules")
+devtools::load_all("model_code/housingledmodel")
 
 #Setup
 projection_name <- "2018_based_shlaa_constrained"
@@ -56,6 +57,6 @@ config_list <- list(
 
 #---------------------
 #run projection
-source('model_code/model_scripts/housing_led/housing_led_control.R')
+
 projection <- run_housing_led_model(config_list)
 log_warnings(paste0(output_dir,"warnings.txt"))
