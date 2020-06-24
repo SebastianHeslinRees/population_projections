7b2746f4dc9b6083360ca03c95acb244fc0e08fc
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-06-24 14:48:28 +0100

    process 2019 mye and internal migration datasets into model inputs

---
+++ input_data_scripts/domestic_migration/domestic_migration_2019.R
@@ -0,0 +1,190 @@
+library(dplyr)
+library(data.table)
+
+data_location <- "Q:/Teams/D&PA/Data/domestic_migration/current_series_from_2002/"
+output_location <- "input_data/domestic_migration/2019/"
+
+#--------------------------
+
+file_1 <- fread(paste0(data_location, "raw_data/y2019/Detailed_Estimates_2019_LA_2019_Dataset_1.csv"))
+file_2 <- fread(paste0(data_location, "raw_data/y2019/Detailed_Estimates_2019_LA_2019_Dataset_2.csv"))
+
+past_data <- readRDS("input_data/domestic_migration/2018/domestic_migration_flows_ons.rds")
+
+#----------------------------
+
+#combine and clean 2019 data
+dom_flows <- rbind(file_1, file_2) %>%
+  rename(gss_in = InLA,
+         gss_out = OutLA) %>% 
+  mutate(sex = case_when(Sex == "M" ~ "male",
+                         Sex == "F" ~ "female",
+                         TRUE ~ "NA"),
+         age = ifelse(Age > 90, 90, Age),
+         year = 2019) %>% 
+  group_by(gss_out, gss_in, year, sex, age) %>%
+  summarise(value = sum(Moves)) %>% 
+  data.frame()
+
+
+#checks
+unique(dom_flows$sex)
+range(dom_flows$age)
+unique(substr(dom_flows$gss_in,1,3))
+unique(substr(dom_flows$gss_out,1,3))
+str(dom_flows)
+sum(is.na(dom_flows))
+
+#----------------------------
+
+#set past data & new to same geography
+#this will take a while
+devtools::load_all('model_code/popmodules')
+
+past_data <- past_data %>% 
+  popmodules::recode_gss_codes(col_geog = "gss_in", data_cols = "value",
+                               recode_to_year = 2020) %>%
+  popmodules::recode_gss_codes(col_geog = "gss_out", data_cols = "value",
+                               recode_to_year = 2020)  
+
+dom_flows <- dom_flows %>% 
+  popmodules::recode_gss_codes(col_geog = "gss_in", data_cols = "value",
+                               recode_to_year = 2020) %>%
+  popmodules::recode_gss_codes(col_geog = "gss_out", data_cols = "value",
+                               recode_to_year = 2020) 
+
+popmodules::validate_same_geog(past_data, dom_flows, "gss_in", "gss_in")
+popmodules::validate_same_geog(past_data, dom_flows, "gss_out", "gss_out")
+
+
+#------------------------------------
+
+#combine and save
+dom_flows_output <- rbind(past_data, dom_flows)
+rm(list=setdiff(ls(), c("dom_flows_output","output_location")))
+saveRDS(dom_flows_output, paste0(ouput_location, "domestic_migration_flows_ons_(2020_geog).rds"))
+
+#------------------------------------
+
+#gross at net flows files
+dom_in <- dom_flows_output %>% 
+  group_by(gss_code = gss_in, year, sex, age) %>% 
+  summarise(dom_in = sum(value)) %>% 
+  data.frame() %>% 
+  tidyr::complete(gss_code = unique(dom_flows_output$gss_in),
+                  year = 2002:2019,
+                  sex = c("male","female"),
+                  age = 0:90,
+                  fill = list(dom_in = 0))
+
+dom_out <- dom_flows_output %>% 
+  group_by(gss_code = gss_out, year, sex, age) %>% 
+  summarise(dom_out = sum(value)) %>% 
+  data.frame() %>% 
+  tidyr::complete(gss_code = unique(dom_flows_output$gss_in),
+                  year = 2002:2019,
+                  sex = c("male","female"),
+                  age = 0:90,
+                  fill = list(dom_out = 0))
+
+dom_net <- dom_out %>%
+  mutate(dom_in = dom_out *-1) %>% 
+  select(-dom_out) %>% 
+  rbind(dom_in) %>% 
+  group_by(gss_code, year, sex, age) %>% 
+  summarise(dom_net = sum(dom_in)) %>% 
+  data.frame()
+
+#--------------------------------------
+
+#regional flows
+district_to_region <- readRDS("input_data/lookup/district_to_region.rds")
+
+regional_flows <- dom_flows_output %>%
+  left_join(district_to_region, by=c("gss_out"="gss_code")) %>% 
+  select(-gss_out) %>% 
+  rename(gss_out = region_gss_code) %>% 
+  left_join(district_to_region, by=c("gss_in"="gss_code")) %>% 
+  select(-gss_in) %>% 
+  rename(gss_in = region_gss_code) %>% 
+  filter(gss_in != gss_out) %>% 
+  group_by(gss_in, gss_out, year, sex, age) %>% 
+  summarise(value = sum(value)) %>% 
+  data.frame()
+
+reg_dom_in <- regional_flows %>% 
+  group_by(gss_code = gss_in, year, sex, age) %>% 
+  summarise(dom_in = sum(value)) %>% 
+  data.frame() %>% 
+  tidyr::complete(gss_code = unique(district_to_region$region_gss_code),
+                  year = 2002:2019,
+                  sex = c("male","female"),
+                  age = 0:90,
+                  fill = list(dom_in = 0))
+
+reg_dom_out <- regional_flows %>% 
+  group_by(gss_code = gss_out, year, sex, age) %>% 
+  summarise(dom_out = sum(value)) %>% 
+  data.frame() %>% 
+  tidyr::complete(gss_code = unique(district_to_region$region_gss_code),
+                  year = 2002:2019,
+                  sex = c("male","female"),
+                  age = 0:90,
+                  fill = list(dom_out = 0))
+
+reg_dom_net <- reg_dom_out %>%
+  mutate(dom_in = dom_out *-1) %>% 
+  select(-dom_out) %>% 
+  rbind(reg_dom_in) %>% 
+  group_by(gss_code, year, sex, age) %>% 
+  summarise(dom_net = sum(dom_in)) %>% 
+  data.frame()
+
+#---------------------------------------------------
+
+#national flows
+
+national_flows <- dom_flows_output %>%
+  mutate(gss_in = case_when(substr(gss_in,1,1) == "E" ~ "E92000001",
+                            substr(gss_in,1,1) == "N" ~ "E92000002",
+                            substr(gss_in,1,1) == "S" ~ "E92000003",
+                            substr(gss_in,1,1) == "W" ~ "W92000004")) %>%
+  mutate(gss_out = case_when(substr(gss_out,1,1) == "E" ~ "E92000001",
+                             substr(gss_out,1,1) == "N" ~ "E92000002",
+                             substr(gss_out,1,1) == "S" ~ "E92000003",
+                             substr(gss_out,1,1) == "W" ~ "W92000004")) %>% 
+  filter(gss_in != gss_out) %>% 
+  group_by(gss_in, gss_out, year, sex, age) %>% 
+  summarise(value = sum(value)) %>% 
+  data.frame()
+
+nat_dom_in <- national_flows %>% 
+  group_by(gss_code = gss_in, year, sex, age) %>% 
+  summarise(dom_in = sum(value)) %>% 
+  data.frame() 
+
+nat_dom_out <- national_flows %>% 
+  group_by(gss_code = gss_out, year, sex, age) %>% 
+  summarise(dom_out = sum(value)) %>% 
+  data.frame() 
+
+nat_dom_net <- nat_dom_out %>%
+  mutate(dom_in = dom_out *-1) %>% 
+  select(-dom_out) %>% 
+  rbind(nat_dom_in) %>% 
+  group_by(gss_code, year, sex, age) %>% 
+  summarise(dom_net = sum(dom_in)) %>% 
+  data.frame()
+
+#---------------------------------------------------
+
+dom_in_all <- rbind(dom_in, reg_dom_in, nat_dom_in)
+dom_out_all <- rbind(dom_out, reg_dom_out, nat_dom_out)
+dom_net_all <- rbind(dom_net, reg_dom_net, nat_dom_net)
+
+saveRDS(dom_in_all, paste0(output_location, "domestic_migration_in_(2020_geog).rds"))
+saveRDS(dom_out_all, paste0(output_location, "domestic_migration_out_(2020_geog).rds"))
+saveRDS(dom_net_all, paste0(output_location, "domestic_migration_net_(2020_geog).rds"))
+
+saveRDS(national_flows, paste0(output_location, "national_domestic_migration_flows.rds"))
+saveRDS(regional_flows, paste0(output_location, "regional_domestic_migration_flows.rds"))
---
+++ input_data_scripts/mye/ons_mye_2019.R
@@ -0,0 +1,49 @@
+library(dplyr)
+
+data_location <- "Q:/Teams/D&PA/Data/population_estimates/ons_mid_year_estimates/current_series/mye_2019/"
+output_location <- "input_data/mye/2019/"
+
+all_components <- readRDS(paste0(data_location, "mye_2019_detailed_components_of_change_(2020_geog).rds"))
+
+#----------------------
+
+unique(all_components$component)
+
+split_components <- function(data, comp, data_col=comp){
+  
+  x <- filter(data, component == comp) %>%
+    select(gss_code, year, sex, age, value) %>%
+    rename(!!sym(data_col) := value)
+}
+
+population <- split_components(all_components, "population", "popn")
+births <- split_components(all_components, "births")
+deaths <-  split_components(all_components, "deaths")
+dom_in <- split_components(all_components, "internal_in", "dom_in")
+dom_out <- split_components(all_components, "internal_out", "dom_out")
+dom_net <- split_components(all_components, "internal_net", "dom_net")
+int_in <- split_components(all_components, "international_in", "int_in")
+int_out <- split_components(all_components, "international_out", "int_out")
+int_net <- split_components(all_components, "international_net", "int_net")
+
+other <- rbind(split_components(all_components, "unattrib", "upc"),
+               split_components(all_components, "other_adjust", "upc"),
+               split_components(all_components, "special_change", "upc")) %>%
+  group_by(gss_code, year, sex, age) %>% 
+  summarise(upc = sum(upc)) %>% 
+  data.frame()
+
+#------------
+
+saveRDS(population, paste0(output_location, "population_ons.rds"))
+saveRDS(births, paste0(output_location, "births_ons.rds"))
+saveRDS(deaths, paste0(output_location, "deaths_ons.rds"))
+saveRDS(dom_in, paste0(output_location, "dom_in_ons.rds"))
+saveRDS(dom_out, paste0(output_location, "dom_out_ons.rds"))
+saveRDS(dom_net, paste0(output_location, "dom_net_ons.rds"))
+saveRDS(int_in, paste0(output_location, "int_in_ons.rds"))
+saveRDS(int_out, paste0(output_location, "int_out_ons.rds"))
+saveRDS(int_net, paste0(output_location, "int_net_ons.rds"))
+saveRDS(dom_in, paste0(output_location, "dom_in_ons.rds"))
+saveRDS(dom_out, paste0(output_location, "dom_out_ons.rds"))
+saveRDS(other, paste0(output_location, "upc_ons.rds"))
