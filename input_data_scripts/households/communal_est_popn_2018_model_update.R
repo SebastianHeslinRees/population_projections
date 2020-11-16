07b3ae61d1dd9499abe7b10485224d118001cf34
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-11-16 10:20:33 +0000

    fixes to updated ce pop to account for issues in the input data

--- input_data_scripts/households/communal_est_popn_2018_model_update.R
+++ input_data_scripts/households/communal_est_popn_2018_model_update.R
@@ -10,8 +10,10 @@ ce <- rbind(data.table::fread(paste0(ons_data_location, "ce_population_female.cs
          sex = case_when(sex == "Female" ~ "female",
                          sex == "Male" ~ "male")) %>%
   select(-district) %>%
+  filter(age_group != "All ages") %>% 
   popmodules::recode_gss_codes(data_cols = "ce_pop",
-                               recode_gla_codes = TRUE)
+                               col_geog="gss_code",
+                               recode_to_year = 2020)
 
 hh_pop <- rbind(data.table::fread(paste0(ons_data_location, "hh_population_female.csv"), header = T),
                 data.table::fread(paste0(ons_data_location, "hh_population_male.csv"), header = T)) %>%
@@ -21,11 +23,15 @@ hh_pop <- rbind(data.table::fread(paste0(ons_data_location, "hh_population_femal
          sex = case_when(sex == "Female" ~ "female",
                          sex == "Male" ~ "male")) %>%
   select(-district) %>%
+  filter(age_group != "All ages") %>% 
   popmodules::recode_gss_codes(data_cols = "hh_pop",
-                               recode_gla_codes = TRUE)
+                               col_geog="gss_code",
+                               recode_to_year = 2020)
 
 ce <- left_join(ce, hh_pop, by = c("gss_code", "age_group", "sex", "year")) %>%
   mutate(ce_rate = ifelse(age_group %in% c("75_79","80_84","85_89","90+"), ce_pop/(ce_pop+hh_pop), NA)) %>%
   select(-hh_pop)
 
 saveRDS(ce, "input_data/household_model/ons_communal_establishment_population_2018_(2020_model).rds")
+
+rm(list = ls())
