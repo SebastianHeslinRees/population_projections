c04215c842c7b6ad0708684f1b1b4c88fdf2d329
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2020-12-01 10:07:37 +0000

    process revised gla mye into model inputs. model covid-19 deaths.

--- input_data_scripts/mortality/mortality_rates_2019.R
+++ input_data_scripts/mortality/mortality_rates_2019.R
@@ -2,6 +2,8 @@ library(popmodules)
 library(dplyr)
 
 popn_mye_path <- paste0("input_data/mye/2019/population_ons.rds")
+gla_popn_mye_path <- paste0("input_data/mye/2019/population_gla.rds")
+
 births_mye_path <-  paste0("input_data/mye/2019/births_ons.rds")
 deaths_mye_path <-  paste0("input_data/mye/2019/deaths_ons.rds")
 
@@ -49,4 +51,52 @@ trend_5yrs <- scaled_mortality_curve(popn_mye_path = popn_mye_path,
 
 saveRDS(average_5yrs, "input_data/mortality/mort_rates_5yr_avg_2019.rds")
 saveRDS(trend_5yrs, "input_data/mortality/mort_rates_5yr_trend_2019.rds")
+rm(average_5yrs, trend_5yrs)
 
+#---
+
+# GLA population denominators
+
+#---- 5 year average rates
+
+average_5yrs <- scaled_mortality_curve(popn_mye_path = gla_popn_mye_path,
+                                       births_mye_path = births_mye_path,
+                                       deaths_mye_path = deaths_mye_path,
+                                       target_curves_filepath = mortality_curve_filepath,
+                                       last_data_year = 2019,
+                                       n_years_to_avg = 5,
+                                       avg_or_trend = "average",
+                                       data_col = "deaths",
+                                       output_col = "rate") %>% 
+  
+  project_rates_npp(rate_col = "rate",
+                    rate_trajectory_filepath = mortality_trajectory_filepath,
+                    first_proj_yr = 2020,
+                    n_proj_yr = 31,
+                    npp_var = "2018_principal")
+
+
+#--- 5 year trended rates
+
+trend_5yrs <- scaled_mortality_curve(popn_mye_path = gla_popn_mye_path,
+                                     births_mye_path = births_mye_path,
+                                     deaths_mye_path = deaths_mye_path,
+                                     target_curves_filepath = mortality_curve_filepath,
+                                     last_data_year = 2019,
+                                     n_years_to_avg = 5,
+                                     avg_or_trend = "trend",
+                                     data_col = "deaths",
+                                     output_col = "rate") %>% 
+  
+  project_rates_npp(rate_col = "rate",
+                    rate_trajectory_filepath = mortality_trajectory_filepath,
+                    first_proj_yr = 2020,
+                    n_proj_yr = 31,
+                    npp_var = "2018_principal")
+
+#--- Save
+
+saveRDS(average_5yrs, "input_data/mortality/mort_rates_5yr_avg_2019_gla_mye.rds")
+saveRDS(trend_5yrs, "input_data/mortality/mort_rates_5yr_trend_2019_gla_mye.rds")
+
+rm(list = ls())
