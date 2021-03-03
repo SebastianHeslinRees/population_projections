5f177a0eada6d9488c10efb2af811fe6ef83dc9b
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-03-03 11:36:11 +0000

    bpo config

---
+++ config_scripts/housing_led/bpo_configs/hounslow.R
@@ -0,0 +1,5 @@
+library(housingledmodel)
+
+bpo_name <- run_bpo_projection(bpo_name = "hounslow",
+                               trajectory_range = 2021:2034,
+                               variant = "low")
