b8a4227360256c1590f37db282105d88217bc234
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-03-23 18:38:32 +0000

    bpo configs

---
+++ config_scripts/housing_led/bpo_configs/city_of_london.R
@@ -0,0 +1,5 @@
+library(housingledmodel)
+
+bpo_name <- run_bpo_projection(bpo_name = "city_of_london",
+                               trajectory_range = 2020:2036,
+                               variant = scenario)
---
+++ config_scripts/housing_led/bpo_configs/lewisham.R
@@ -0,0 +1,5 @@
+library(housingledmodel)
+
+bpo_name <- run_bpo_projection(bpo_name = "wandsworth",
+                               trajectory_range = 2020:2035,
+                               variant = scenario)
