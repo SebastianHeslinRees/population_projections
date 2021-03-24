b898de90306ac6c5d941ddfe78e9d2663b99187b
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-03-24 14:33:41 +0000

    bpo config updates

--- config_scripts/housing_led/bpo_configs/croydon.R
+++ config_scripts/housing_led/bpo_configs/croydon.R
@@ -1,7 +1,7 @@
 library(housingledmodel)
 
 bpo_name <- run_bpo_projection(bpo_name = "croydon",
-                               trajectory_range = 2020:2038,
+                               trajectory_range = 2020:2039,
                                variant = scenario)
 
 
--- config_scripts/housing_led/bpo_configs/kingston_upon_thames.R
+++ config_scripts/housing_led/bpo_configs/kingston_upon_thames.R
@@ -1,5 +1,5 @@
 library(housingledmodel)
 
 bpo_name <- run_bpo_projection(bpo_name = "kingston_upon_thames",
-                               trajectory_range = 2020:2041,
+                               trajectory_range = 2019:2041,
                                variant = scenario)
