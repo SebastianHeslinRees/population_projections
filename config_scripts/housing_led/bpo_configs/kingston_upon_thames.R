da23ed0e92460ce0dff888c94dc7b9b3d8780028
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-03-25 15:18:59 +0000

    final tweaks

--- config_scripts/housing_led/bpo_configs/kingston_upon_thames.R
+++
@@ -1,5 +0,0 @@
-library(housingledmodel)
-
-bpo_name <- run_bpo_projection(bpo_name = "kingston_upon_thames",
-                               trajectory_range = 2019:2041,
-                               variant = scenario)
