ab690be8aa2abfcc2ddfdc2fa1924300d6ec734e
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-04-06 10:23:27 +0100

    some unneccessary configs removed

--- config_scripts/housing_led/other/greenwich_scenario_1_med.R
+++
@@ -1,8 +0,0 @@
-library(housingledmodel)
-
-bpo_name <- run_bpo_projection(bpo_name = "greenwich_scenario_1",
-                               shlaa_first_yr = 2035,
-                               last_proj_yr = 2050,
-                               dev_first_yr = 2020,
-                               migration_scenario = "medium")
-
--- config_scripts/housing_led/other/greenwich_scenario_1_parallel.R
+++
@@ -1,7 +0,0 @@
-library(housingledmodel)
-
-for(sc in c("high","med","low")){
-  rstudioapi::jobRunScript(paste0('model_code/config_scripts/housing_led/bpo_configs/greenwich_scenario_1_',sc,".R"),
-                           name = sc, encoding = "unknown",
-                           workingDir = ".", importEnv = FALSE, exportEnv = "")
-}
