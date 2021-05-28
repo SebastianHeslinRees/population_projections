544f213d9bd37f4137d19d1b1b7e1974f915375e
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-05-28 10:28:18 +0100

    islington bpo config

---
+++ config_scripts/housing_led/bpo_configs/islington.R
@@ -0,0 +1,5 @@
+library(housingledmodel)
+
+bpo_name <- run_bpo_projection(bpo_name = "islington",
+                               trajectory_range = 2012:2036,
+                               variant = scenario)
