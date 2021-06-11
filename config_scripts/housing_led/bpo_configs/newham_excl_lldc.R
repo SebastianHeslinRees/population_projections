d88fa5e8db9903f54945530b13bbd61d110a3fa3
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-06-11 13:15:45 +0100

    bpo configs

---
+++ config_scripts/housing_led/bpo_configs/newham_excl_lldc.R
@@ -0,0 +1,6 @@
+library(housingledmodel)
+
+bpo_name <- run_bpo_projection(bpo_name = "newham_excl_lldc",
+                               trajectory_range = 2019:2035,
+                               variant = scenario)
+
