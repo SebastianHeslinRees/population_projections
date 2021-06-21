11dc472c1811ed5836c89adc876118663602deac
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-06-21 12:21:54 +0100

    barnet bpo config

---
+++ config_scripts/housing_led/bpo_configs/barnet.R
@@ -0,0 +1,6 @@
+library(housingledmodel)
+
+bpo_name <- run_bpo_projection(bpo_name = "barnet",
+                               trajectory_range = 2021:2036,
+                               variant = scenario)
+
