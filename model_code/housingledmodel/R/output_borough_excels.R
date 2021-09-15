2ebae461104c8abbe87ca18afd235332e7d8ed96
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-09-15 15:48:52 +0100

    changes to excel fns so they work outside the models

--- model_code/housingledmodel/R/output_borough_excels.R
+++ model_code/housingledmodel/R/output_borough_excels.R
@@ -3,8 +3,7 @@
 #' Process population, components and development data and place it into an
 #' Excel template. London borough data.
 #'
-#' @param data The \code{[['csvs']]} element from a ward projection
-#' @param output_dir The directory in which to save the Excel file
+#' @param output_dir The directory from which to read the data and save the Excel file
 #' @param projection_name The name of the output Excel file
 #' @param file_suffix A string to append to the end of the file name
 #' 
@@ -14,40 +13,11 @@
 #' 
 #' @export
 
-output_borough_excels <- function(data, output_dir, projection_name, file_suffix = ".xlsx"){
+output_borough_excels <- function(output_dir, projection_name, file_suffix = ".xlsx"){
   
-  prep_data <- function(x, col_aggregation = c("gss_code", "borough", "sex", "age")){
-    
-    if("ward_name" %in% names(x)){
-      x <- x %>%
-        as.data.frame() %>%
-        select(-gss_code_ward, -ward_name)
-    }
-    
-    y <- x %>% 
-      as.data.frame() %>%
-      mutate(gss_code = "E12000007",
-             borough = "London (total)") %>% 
-      dtplyr::lazy_dt() %>%
-      group_by_at(col_aggregation) %>%
-      summarise_all(.funs=sum) %>%
-      as.data.frame()
-    
-    z <- x %>% 
-      dtplyr::lazy_dt() %>%
-      group_by_at(col_aggregation) %>%
-      summarise_all(.funs=sum) %>%
-      as.data.frame()
-    
-    return(rbind(y,z))
-    
-  }
-  
-  #-----------------------------------------------------------------------------
-  
-  persons <- data[["persons"]] %>% prep_data()
-  females <- data[["females"]] %>% prep_data()
-  males <- data[["males"]] %>% prep_data()
+  persons <- fread(paste0(output_dir,"csv/persons.csv"), header = TRUE) %>% as.data.frame() 
+  females <- fread(paste0(output_dir,"csv/females.csv"), header = TRUE) %>% as.data.frame() 
+  males <- fread(paste0(output_dir,"csv/males.csv"), header = TRUE) %>% as.data.frame() 
   
   components <- fread(paste0(output_dir,"csv/components.csv"), header = TRUE) %>% 
     as.data.frame() %>% 
@@ -72,7 +42,7 @@ output_borough_excels <- function(data, output_dir, projection_name, file_suffix
   addDataFrame(stock_dataframe, wb_sheets$`housing stock`, col.names = FALSE, row.names = FALSE, startRow = 2, startColumn = 1)
   
   #-----------------------------------------------------------------------------
-
+  
   #Write borough file
   if(substr(file_suffix, nchar(file_suffix)-4, nchar(file_suffix)) != ".xlsx"){
     file_suffix <- paste0(file_suffix,".xlsx")
@@ -81,5 +51,5 @@ output_borough_excels <- function(data, output_dir, projection_name, file_suffix
   dir.create(output_dir, showWarnings = FALSE)
   wb_filename <- paste0(output_dir, projection_name, file_suffix)
   xlsx::saveWorkbook(wb, wb_filename)
-
+  
 }
--- model_code/trendmodel/R/create_trend_model_excels.R
+++ model_code/trendmodel/R/create_trend_model_excels.R
@@ -15,27 +15,31 @@
 #'   trend_dir = "outputs/trend/2018/2018_long_19-11-13_2144",
 #'   excel_file_name = "2018_long_trend.xlsx")
 #' }
+#' 
+#' @export
 
-create_trend_model_excels <- function(trend_dir, excel_file_name){
+create_trend_model_excels <- function(trend_dir, excel_file_name, household_models = TRUE){
   
   if(substr(excel_file_name, nchar(excel_file_name)-4, nchar(excel_file_name)) != ".xlsx"){
     excel_file_name <- paste0(excel_file_name,".xlsx")
   }
-
+  
   trend_datastore_outputs(population = readRDS(paste0(trend_dir,"population.rds")),
-                    births = readRDS(paste0(trend_dir,"births.rds")),
-                    deaths = readRDS(paste0(trend_dir,"deaths.rds")),
-                    int_in = readRDS(paste0(trend_dir,"int_in.rds")),
-                    int_out = readRDS(paste0(trend_dir,"int_out.rds")),
-                    dom_in = readRDS(paste0(trend_dir,"dom_in.rds")),
-                    dom_out = readRDS(paste0(trend_dir,"dom_out.rds")),
-                    popn_adjustment =  readRDS(paste0(trend_dir,"popn_adjustment.rds")),
-                    output_dir = trend_dir,
-                    excel_file_name = excel_file_name)
-
-  for(model in c("ons","dclg")){
-    household_model_outputs(model_output = readRDS(paste0(trend_dir, model, "_households.rds")),
-                            model = model, output_dir = trend_dir, write_excel = T)
+                          births = readRDS(paste0(trend_dir,"births.rds")),
+                          deaths = readRDS(paste0(trend_dir,"deaths.rds")),
+                          int_in = readRDS(paste0(trend_dir,"int_in.rds")),
+                          int_out = readRDS(paste0(trend_dir,"int_out.rds")),
+                          dom_in = readRDS(paste0(trend_dir,"dom_in.rds")),
+                          dom_out = readRDS(paste0(trend_dir,"dom_out.rds")),
+                          popn_adjustment =  readRDS(paste0(trend_dir,"popn_adjustment.rds")),
+                          output_dir = trend_dir,
+                          excel_file_name = excel_file_name)
+  
+  if(household_models){
+    for(model in c("ons","dclg")){
+      household_model_outputs(model_output = readRDS(paste0(trend_dir, "households/", model, "_households.rds")),
+                              model = model, output_dir = trend_dir, write_excel = T)
+    }
   }
-
+  
 }
