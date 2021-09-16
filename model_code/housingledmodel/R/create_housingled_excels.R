a37f6efc8e1730e4e59cccf97b76815798462875
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-09-16 18:29:17 +0100

    housingled python script

---
+++ model_code/housingledmodel/R/create_housingled_excels.R
@@ -0,0 +1,56 @@
+#' Create the 2020-based housing-led projection excel output
+#' 
+#' Process population, components and development data and place it into an
+#' Excel template. London borough data.
+#'
+#' @param output_dir The directory from which to read the data and save the Excel file
+#' @param projection_name The name of the output Excel file
+#' @param file_suffix A string to append to the end of the file name
+#' 
+#' @import dplyr
+#' @importFrom data.table fread
+#' 
+#' @export
+
+create_housingled_excels <- function(output_dir, projection_name, file_suffix = ".xlsx"){
+  
+  if(substr(output_dir,nchar(output_dir),nchar(output_dir))!="/"){
+    output_dir <- paste0(output_dir,"/")
+  }
+  
+  persons <- fread(paste0(output_dir,"csv/persons.csv"), header = TRUE) %>% as.data.frame() 
+  females <- fread(paste0(output_dir,"csv/females.csv"), header = TRUE) %>% as.data.frame() 
+  males <- fread(paste0(output_dir,"csv/males.csv"), header = TRUE) %>% as.data.frame() 
+  
+  components <- fread(paste0(output_dir,"csv/components.csv"), header = TRUE) %>% 
+    as.data.frame() %>% 
+    rename(population = popn)
+  
+  names(components) <- stringr::str_replace_all(names(components), "_", " ")
+  
+  assumed_dev <- fread(paste0(output_dir,"csv/assumed_dev.csv"), header = TRUE)
+  stock <- fread(paste0(output_dir,"csv/housing_stock.csv"), header = TRUE)
+  
+
+  if(substr(file_suffix, nchar(file_suffix)-4, nchar(file_suffix)) != ".xlsx"){
+    file_suffix <- paste0(file_suffix,".xlsx")
+  }
+  
+  output_dir <- paste0(output_dir, "excels/")
+  dir.create(output_dir, showWarnings = FALSE)
+  wb_filename <- paste0(projection_name, file_suffix)
+ 
+  #python excel
+  #TODO
+  #FIXME
+  #There is a problem with reticulate/python/renv that I can't get to the bottom of
+  #The first time a python script is sourced the function throws an error. It can't find
+  #rpytools. If you source again it works. The temp work around is to wrap the source in
+  #try() so the error is caught and then run it a second time. Its ugly but it works.
+  #See also smallareamodel::output_small_area_excels
+  
+  try( reticulate::source_python('model_code/other_scripts/python_to_excel_housingled.py'), silent = TRUE)
+  reticulate::source_python('model_code/other_scripts/python_to_excel_housingled.py') 
+  python_to_excel_housingled(persons, females, males, components, assumed_dev, stock, output_dir, wb_filename)
+  
+}
