1a8da74a572bf14afaad8fb83d3120d714f90b81
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-11-18 13:51:09 +0000

    same-geog

--- model_code/popmodules/R/validate_same_geography.R
+++ model_code/popmodules/R/validate_same_geography.R
@@ -28,42 +28,52 @@ validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
   assert_that(error_or_warn %in% c("warn","error"),
               msg = paste0("in validte_same_geog, param error_or_warn is '",
                            error_or_warn, "' - must be 'error' or 'warn'"))
-  assert_that(col_1 %in% names(df_1),
+  assert_that(all(col_1 %in% names(df_1)),
               msg = paste0("in validte_same_geog, col_1 not in df_1"))
-  assert_that(col_2 %in% names(df_2),
+  assert_that(all(col_2 %in% names(df_2)),
               msg = paste0("in validte_same_geog, col_2 not in df_2"))
-  
+  assert_that(length(col_1)==length(col_2),
+              msg = "in validate_same_geog vectors of different lengths passed to col_1 and col_2")
+  browser()
   #check that the geography columns contain the same set of codes
-  are_equal <- setequal(df_1[[col_1]], df_2[[col_2]])
-  
-  #if they don't then output useful info about the differences
-  if(!are_equal){
+  for(i in 1:length(col_1)){
     
-    nm_1 <- deparse(substitute(df_1))
-    nm_2 <- deparse(substitute(df_2))
+    are_equal <- setequal(df_1[[col_1[i]]], df_2[[col_2[i]]])
     
-    df_1 <- df_1[all_of(col_1)] %>% unique() %>% rename("gss_code___" = col_1)
-    df_2 <- df_2[all_of(col_2)] %>% unique() %>% rename("gss_code___" = col_2)
-    
-    x <- setdiff(df_1$gss_code___, df_2$gss_code___)
-    if(length(x)>0 && error_or_warn == "warn"){
-      warning(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
+    if(!are_equal){
+      same_geog_report(df_1, df_2, col_1[i], col_2[i])
     }
-    if(length(x)>0 && error_or_warn == "error"){
-      stop(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
-    }
-    
-    
-    y <- setdiff(df_2$gss_code___, df_1$gss_code___)
-    if(length(y)>0 && error_or_warn == "warn"){
-      warning(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
-    }
-    if(length(y)>0 && error_or_warn == "error"){
-      stop(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
-    }
-    
   }
   
   invisible()
+  
+}
+
+#if they don't match then output useful info about the differences
+
+same_geog_report <- function(df_1, df_2, col_1, col_2){
+  
+  nm_1 <- deparse(substitute(df_1))
+  nm_2 <- deparse(substitute(df_2))
+  
+  df_1 <- df_1[all_of(col_1)] %>% unique() %>% rename("gss_code___" = col_1)
+  df_2 <- df_2[all_of(col_2)] %>% unique() %>% rename("gss_code___" = col_2)
+  
+  x <- setdiff(df_1$gss_code___, df_2$gss_code___)
+  if(length(x)>0 && error_or_warn == "warn"){
+    warning(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
+  }
+  if(length(x)>0 && error_or_warn == "error"){
+    stop(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
+  }
+  
+  
+  y <- setdiff(df_2$gss_code___, df_1$gss_code___)
+  if(length(y)>0 && error_or_warn == "warn"){
+    warning(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
+  }
+  if(length(y)>0 && error_or_warn == "error"){
+    stop(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
+  }
   
 }
