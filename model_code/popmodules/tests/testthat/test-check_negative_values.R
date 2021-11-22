1e59e60a6afd6f4f02ad16dca92e10cb858508b3
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-11-22 12:15:45 +0000

    improve implementation of check_negative_values

--- model_code/popmodules/tests/testthat/test-check_negative_values.R
+++ model_code/popmodules/tests/testthat/test-check_negative_values.R
@@ -1,15 +1,19 @@
 library(popmodules)
+library(testthat)
 
 popn <- data.frame(gss_code = letters[1:5], popn = -2:2)
 fixed <- data.frame(gss_code = letters[1:5], popn = c(0, 0, 0, 1, 2))
+fixed_2 <- data.frame(gss_code = letters[1:5], popn = c(5, 5, 0, 1, 2))
 
-
-test_that("check_negative_values works", {
-  expect_warning(out <- check_negative_values(popn, "popn", set_to_zero = FALSE))
+test_that("check_negative_values", {
+  expect_warning(out <- check_negative_values(popn, "popn", warn_only = TRUE))
   expect_equal(out, popn)
 
-  expect_warning(out <- check_negative_values(popn, "popn", set_to_zero = TRUE))
+  expect_warning(out <- check_negative_values(popn, "popn", warn_only = FALSE))
   expect_equal(out, fixed)
 
-  expect_equal(fixed, check_negative_values(fixed, "popn", set_to_zero = TRUE))
+  expect_equal(fixed, check_negative_values(fixed, "popn", warn_only = FALSE))
+  
+  expect_warning(out <- check_negative_values(popn, "popn", change_value = 5))
+  expect_equal(out, fixed_2)
 })
