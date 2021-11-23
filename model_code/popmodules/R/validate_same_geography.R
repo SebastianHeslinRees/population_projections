f7c13f40314d33b7bb1bae37c224552783551a96
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2021-11-23 15:56:36 +0000

    validate-same-geog can handle more than one column

--- model_code/popmodules/R/validate_same_geography.R
+++ model_code/popmodules/R/validate_same_geography.R
@@ -28,49 +28,61 @@ validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
   assert_that(is.logical(warn_only),
               msg = paste0("in validte_same_geog, param warn_only is '",
                            warn_only, "' - must be 'TRUE' or 'FALSE'"))
-  assert_that(col_1 %in% names(df_1),
+  assert_that(all(col_1 %in% names(df_1)),
               msg = paste0("in validte_same_geog, col_1 not in df_1"))
-  assert_that(col_2 %in% names(df_2),
+  assert_that(all(col_2 %in% names(df_2)),
               msg = paste0("in validte_same_geog, col_2 not in df_2"))
+  assert_that(length(col_1) == length(col_2),
+              msg = "in validate_same_grography, col_1 and col_2 must be the same length")
   
   #check that the geography columns contain the same set of codes
-  are_equal <- setequal(df_1[[col_1]], df_2[[col_2]])
-  
-  #if they don't then output useful info about the differences
-  if(!are_equal){
+  for(i in 1:length(col_1)){
+    are_equal <- setequal(df_1[[col_1[i]]], df_2[[col_2[i]]])
     
-    nm_1 <- deparse(substitute(df_1))
-    nm_2 <- deparse(substitute(df_2))
-    
-    df_1 <- df_1[all_of(col_1)] %>% unique() %>% rename("gss_code___" = col_1)
-    df_2 <- df_2[all_of(col_2)] %>% unique() %>% rename("gss_code___" = col_2)
-    
-    #-----------------
-    
-    x <- setdiff(df_1$gss_code___, df_2$gss_code___)
-    
-    if(length(x)>0 && warn_only){
-      warning(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
+    #if they don't then output useful info about the differences
+    if(!are_equal){
+      
+      nm_1 <- deparse(substitute(df_1))
+      nm_2 <- deparse(substitute(df_2))
+      
+      df_1 <- df_1[all_of(col_1)] %>% unique() %>% rename("gss_code___" = col_1[i])
+      df_2 <- df_2[all_of(col_2)] %>% unique() %>% rename("gss_code___" = col_2[i])
+      
+      .validate_same_geog_report(nm_1, nm_2, df_1, df_2, warn_only)
+      
     }
-    
-    if(length(x)>0 && !warn_only){
-      stop(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
-    }
-    
-    #-----------------
-    
-    y <- setdiff(df_2$gss_code___, df_1$gss_code___)
-    
-    if(length(y)>0 && warn_only){
-      warning(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
-    }
-    
-    if(length(y)>0 && !warn_only){
-      stop(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
-    }
-    
   }
   
   invisible()
+}
+#-----------------
+
+.validate_same_geog_report <- function(nm_1, nm_2, df_1, df_2, warn_only){
+  
+  x <- setdiff(df_1$gss_code___, df_2$gss_code___)
+  
+  #-----------------
+  
+  if(length(x)>0 && warn_only){
+    warning(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
+  }
+  
+  if(length(x)>0 && !warn_only){
+    stop(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
+  }
+  
+  #-----------------
+  
+  y <- setdiff(df_2$gss_code___, df_1$gss_code___)
+  
+  if(length(y)>0 && warn_only){
+    warning(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
+  }
+  
+  if(length(y)>0 && !warn_only){
+    stop(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
+  }
   
 }
+
+
--- model_code/popmodules/tests/testthat/test-validate_same_geog.R
+++ model_code/popmodules/tests/testthat/test-validate_same_geog.R
@@ -6,6 +6,10 @@ test_df_3 <- data.frame(code = c("a","b","c"), value = 1:3)
 test_df_4 <- data.frame(gss = c("c","b","a"), value = 1:3)
 test_df_5 <- data.frame(gss = c("a","a","a"), value = 1:3)
 
+test_df_6 <- data.frame(gss_code = c(rep("a",3), rep("b",3), rep("c",3)),
+                        gss_code_ward = letters[15:23])
+test_df_7 <- test_df_6[-1,]
+
 test_that("validate_same_geography basic operation", {
   expect_silent(validate_same_geog(test_df_1, test_df_2, col_1="gss"))
   expect_silent(validate_same_geog(test_df_1, test_df_3, col_1="gss", col_2 = "code"))
@@ -24,3 +28,10 @@ test_that("validate_same_geography throws warnings as expected", {
   expect_warning(validate_same_geog(test_df_1, test_df_5, col_1="gss", warn_only = TRUE))
   expect_warning(validate_same_geog(test_df_5, test_df_1, col_1="gss", warn_only = TRUE))
 })
+
+test_that("validate_same_geography works with multiple geog cols", {
+  expect_warning(validate_same_geog(test_df_6, test_df_7, col_1=c("gss_code","gss_code_ward"), warn_only = TRUE))
+  expect_error(validate_same_geog(test_df_6, test_df_7, col_1=c("gss_code","gss_code_ward"), warn_only = FALSE))
+  expect_silent(validate_same_geog(test_df_6, test_df_6, col_1=c("gss_code","gss_code_ward")))
+})
+
