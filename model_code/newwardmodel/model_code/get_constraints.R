faccc614d57e4806f4ba35438aaeecc974798075
wiltonkiss <william.tonkiss@london.gov.uk>
2022-01-10 08:55:53 +0000

    get constraints

--- model_code/newwardmodel/model_code/get_constraints.R
+++ model_code/newwardmodel/model_code/get_constraints.R
@@ -1,3 +1,36 @@
+#' Read in constraint data a place in a list for use in the small area model
+#'
+#' The constraint_list parameter has 3 elements: the location of a LA-level trend
+#' projection; a mapping parameter which maps between the constraint data and the
+#' projection data; a list of which components to constrain. The function returns
+#' a list of data frames containing the constraint data.
+#' 
+#' @param constraint_list List with 3 elements.
+#'  1: constraint_path: String. The path to the output folder of an LA-level
+#'  projection run using the GLA trend model
+#'  2: mapping: Character. A vector of column names common to both the constraint
+#'  and the projection. E.g. c("gss_code","year","sex","age")
+#'  3: constraint_list: A list of logical values indicating whether or not each
+#'  component should be constrained. The list must include the following components:
+#'  births, deaths, in_migration, out_migration, population)
+#' @param last_proj_yr Numeric. The final projection year.
+#' 
+#' @return A list where each element is a data frame containing constraint data.
+#'
+#' @import popmodules
+#' 
+#' @export
+#'
+#' @example 
+#' get_constraints(
+#'   constraint_path = list(constraint_path = "outputs/trend/2020/2020_CH_central_lower_21-09-21_1259/",
+#'   mapping = c("gss_code","year","sex","age"),
+#'   components = list(births = TRUE,
+#'                     deaths = TRUE,
+#'                     in_migration = FALSE,
+#'                     out_migration = FALSE,
+#'                     population = FALSE))
+
 get_constraints <- function(constraint_list, last_proj_yr){
   
   path <- constraint_list$constraint_path
