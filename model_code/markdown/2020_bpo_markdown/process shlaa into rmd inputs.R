098002abbe624c2b8089447842332e1b3c77825c
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2022-03-22 12:18:36 +0000

    final bpo markdown tweaks

--- model_code/markdown/2020_bpo_markdown/process shlaa into rmd inputs.R
+++ model_code/markdown/2020_bpo_markdown/process shlaa into rmd inputs.R
@@ -77,7 +77,7 @@ shlaa_rmd_2020 <- function(borough_name, wards){
 
 wards <- "WD13"
 
-projections <- list.dirs("outputs/smallarea_model", recursive = FALSE)
+projections <- list.dirs("outputs/flexibleareamodel", recursive = FALSE)
 projections <- projections[grepl(wards, projections)]
 
 upper_path <- projections[grepl("upper", tolower(projections))]
@@ -92,7 +92,7 @@ for(x in c("population","births","deaths","in_migration","out_migration","net_mi
   lower_proj[[x]] <- readRDS(paste0(lower_path, "/", x, ".rds")) 
 }
 
-lookup <- readRDS("input_data/smallarea_model/lookups/ward_2013_name_lookup.rds") %>% 
+lookup <- readRDS("input_data/flexibleareamodel/lookups/ward_2013_name_lookup.rds") %>% 
   filter(substr(gss_code,1,3)=="E09") %>% 
   select(gss_code, la_name) %>%
   distinct()
@@ -105,7 +105,7 @@ for(i in 1:nrow(lookup)){
 
 wards <- "WD22"
 
-projections <- list.dirs("outputs/smallarea_model", recursive = FALSE)
+projections <- list.dirs("outputs/flexibleareamodel", recursive = FALSE)
 projections <- projections[grepl(wards, projections)]
 
 upper_path <- projections[grepl("upper", tolower(projections))]
@@ -120,7 +120,7 @@ for(x in c("population","births","deaths","in_migration","out_migration","net_mi
   lower_proj[[x]] <- readRDS(paste0(lower_path, "/", x, ".rds")) 
 }
 
-lookup <- readRDS("input_data/smallarea_model/lookups/ward_2013_name_lookup.rds") %>% 
+lookup <- readRDS("input_data/flexibleareamodel/lookups/ward_2013_name_lookup.rds") %>% 
   filter(substr(gss_code,1,3)=="E09") %>% 
   select(gss_code, la_name) %>%
   distinct()
