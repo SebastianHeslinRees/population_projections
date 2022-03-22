3ba61d55e94ec14f2627561361bf3dc70f84d682
wiltonkiss <william.tonkiss@london.gov.uk>
2022-03-22 09:33:45 +0000

    tidy up check neg values messages

--- model_code/popmodules/R/check_negative_values.R
+++ model_code/popmodules/R/check_negative_values.R
@@ -29,6 +29,7 @@ check_negative_values <- function(x, data_col, warn_only = FALSE, change_value =
               msg = "check_negative_values expected a dataframe")
   assert_that(is.logical(warn_only),
               msg = paste("check_negative_values expected a non-logical value:", warn_only))
+  assert_that(!any(is.na(x[data_col])), msg = paste("check_negative_values: NAs in", data_col, " column"))
 
   #Function body
   ix <- x[[data_col]] < 0
@@ -47,13 +48,13 @@ check_negative_values <- function(x, data_col, warn_only = FALSE, change_value =
                    n, " aggregation levels summing to ",
                    total_negative))
       print(zero_msg)
-      if(sum(ix) < 30) {
-        print("Values:")
-        print(x[ix,])
-      } else {
-        print("First 30 values:")
-        print(x[ix,][1:30,])
-      }
+      # if(sum(ix) < 30) {
+      #   print("Values:")
+      #   print(x[ix,])
+      # } else {
+      #   print("First 30 values:")
+      #   print(x[ix,][1:30,])
+      # }
     }), collapse = "\n"))
 
     if(!warn_only){
