b9bca9a63a5454a56f3e2599e30cb2ed4dbe45ae
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2022-07-18 12:58:38 +0100

    bpo config

---
+++ config_scripts/flexible_area_model/bpo/waltham_forest.R
@@ -0,0 +1,14 @@
+library(flexibleareamodel); library(job)
+
+bpo_name <- "waltham_forest_WD22"
+
+for(v in c("lower","upper")){
+  job({
+    bpo_name <- run_bpo_projection(bpo_name = bpo_name,
+                                   trajectory_range = 2012:2037,
+                                   wards = "WD22",
+                                   variant = v)},
+    title = paste(bpo_name, v, sep = " - ")
+  )
+}
+
