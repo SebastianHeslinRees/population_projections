803b256f4364b5e461ebd2f94d6d1225f153dca5
Wil Tonkiss <William.Tonkiss@london.gov.uk>
2022-09-06 11:42:11 +0100

    barnet v2 config

--- model_code/other_scripts/bpo_rmd_and_datastore_upload.R
+++ model_code/other_scripts/bpo_rmd_and_datastore_upload.R
@@ -1,9 +1,10 @@
+Sys.sleep(60*60)
 source("model_code/markdown/2020_bpo_markdown/process bpo into rmd inputs.R")
 source("model_code/other_scripts/upload_2020_bpo_excel_rmd.R")
 
-borough <- "havering"
-wards <- "WD22"
-proj_name <- "havering_version_2" #borough
+borough <- "barnet"
+wards <- "WD13"
+proj_name <- "barnet_v2" #borough
 
 #-------------------------------------------------------------------------------
 
