06337e7729145905070bb055440347a540a129ae
wiltonkiss <william.tonkiss@london.gov.uk>
2022-09-28 11:40:01 +0100

    validate nested geography

--- model_code/popmodules/R/validate_same_geography.R
+++ model_code/popmodules/R/validate_same_geography.R
@@ -12,6 +12,8 @@
 #' @param warn_only Logical. Should the function return a warning or an error.
 #'   If set to TRUE the function returns a warning, set to FALSE returns an error.
 #'   Default FALSE. 
+#' @param nested_geog Logical. Should the nested geography be tested to ensure there
+#'  is a many-to-one relationship between lower and higher geographies.
 #'   
 #' @return Warnings or errors if there differences in the gss codes supplied
 #'
@@ -22,7 +24,8 @@
 #' @export
 
 validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
-                               warn_only = FALSE){
+                               warn_only = FALSE,
+                               nested_geog = FALSE){
   #validate function inputs
   assert_that(is.logical(warn_only),
               msg = paste0("in validte_same_geog, param warn_only is '",
@@ -34,6 +37,9 @@ validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
   assert_that(length(col_1) == length(col_2),
               msg = "in validate_same_grography, col_1 and col_2 must be the same length")
   
+  nm_1 <- deparse(substitute(df_1))
+  nm_2 <- deparse(substitute(df_2))
+  
   #check that the geography columns contain the same set of codes
   for(i in 1:length(col_1)){
     are_equal <- setequal(df_1[[col_1[i]]], df_2[[col_2[i]]])
@@ -41,18 +47,21 @@ validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
     #if they don't then output useful info about the differences
     if(!are_equal){
       
-      nm_1 <- deparse(substitute(df_1))
-      nm_2 <- deparse(substitute(df_2))
+      df_1_report <- df_1[all_of(col_1)] %>% unique() %>% rename("gss_code___" = col_1[i])
+      df_2_report <- df_2[all_of(col_2)] %>% unique() %>% rename("gss_code___" = col_2[i])
       
-      df_1 <- df_1[all_of(col_1)] %>% unique() %>% rename("gss_code___" = col_1[i])
-      df_2 <- df_2[all_of(col_2)] %>% unique() %>% rename("gss_code___" = col_2[i])
-      
-      .validate_same_geog_report(nm_1, nm_2, df_1, df_2, warn_only)
+      .validate_same_geog_report(nm_1, nm_2, df_1_report, df_2_report, warn_only)
       
     }
   }
   
-
+  if(nested_geog){
+    
+    .validate_nested_geog(df_1, col_1, nm_1, warn_only)
+    .validate_nested_geog(df_2, col_2, nm_2, warn_only)
+    
+  }
+  
   invisible()
 }
 #-----------------
@@ -64,7 +73,7 @@ validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
   #-----------------
   
   if(length(x)>0 && warn_only){
-    warning(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , x))
+    warning(paste(length(x),"codes in",nm_1,"not in",nm_2, ": " , paste(x, collapse = ", ")))
   }
   
   if(length(x)>0 && !warn_only){
@@ -76,7 +85,7 @@ validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
   y <- setdiff(df_2$gss_code___, df_1$gss_code___)
   
   if(length(y)>0 && warn_only){
-    warning(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , y))
+    warning(paste(length(y),"codes in",nm_2,"not in",nm_1, ": " , paste(y, collapse = ", ")))
   }
   
   if(length(y)>0 && !warn_only){
@@ -86,3 +95,28 @@ validate_same_geog <- function(df_1, df_2, col_1="gss_code", col_2=col_1,
 }
 
 
+.validate_nested_geog <- function(x, nested_cols, nm, warn_only){
+  
+  y <- x %>% 
+    select(all_of(nested_cols)) %>% 
+    distinct() %>% 
+    group_by(across(!!nested_cols[2])) %>% 
+    summarise(n = n())
+  
+  not_unique <- y[y$n > 1,]
+  codes <- not_unique[[nested_cols[2]]]
+  
+  if(nrow(not_unique)>0){
+    
+    nested_msg <- paste0("Nested geography in dataframe ", nm, ": ", nrow(not_unique), " ",
+                         nested_cols[2], " codes appear in more than 1 ", nested_cols[1],
+                         ". Check codes: ", paste(codes, collapse = ", "))
+    
+    if(warn_only){
+      warning(nested_msg)
+    } else {
+      stop(nested_msg)
+    }
+    
+  }
+}
--- model_code/popmodules/tests/testthat/test-validate_same_geog.R
+++ model_code/popmodules/tests/testthat/test-validate_same_geog.R
@@ -10,6 +10,14 @@ test_df_6 <- data.frame(gss_code = c(rep("a",3), rep("b",3), rep("c",3)),
                         gss_code_ward = letters[15:23])
 test_df_7 <- test_df_6[-1,]
 
+test_df_8 <- data.frame(gss_code = c(rep("a",5), rep("b", 5), rep("c", 5), "a", "b"),
+                        gss_code_ward = c(1:15, 15, 12),
+                        value = 1:17)
+
+test_df_9 <- data.frame(gss_code = c(rep("a",5), rep("b", 5), rep("c", 5)),
+                        gss_code_ward = c(1:15),
+                        value = 1:15)
+
 test_that("validate_same_geography basic operation", {
   expect_silent(validate_same_geog(test_df_1, test_df_2, col_1="gss"))
   expect_silent(validate_same_geog(test_df_1, test_df_3, col_1="gss", col_2 = "code"))
@@ -34,3 +42,10 @@ test_that("validate_same_geography works with multiple geog cols", {
   expect_error(validate_same_geog(test_df_6, test_df_7, col_1=c("gss_code","gss_code_ward"), warn_only = FALSE))
   expect_silent(validate_same_geog(test_df_6, test_df_6, col_1=c("gss_code","gss_code_ward")))
 })
+
+test_that("validate_same_geography works with nested_geography", {
+  expect_warning(validate_same_geog(test_df_8, test_df_9, col_1=c("gss_code","gss_code_ward"), nested_geog = TRUE, warn_only = TRUE))
+  expect_error(validate_same_geog(test_df_8, test_df_9, col_1=c("gss_code","gss_code_ward"), nested_geog = TRUE, warn_only = FALSE))
+  expect_silent(validate_same_geog(test_df_8, test_df_9, col_1=c("gss_code","gss_code_ward"), nested_geog = FALSE, warn_only = FALSE))
+})
+
